e1e95421a4f9bb6fd65f8893c2a65f87e61dbeba
Victor Login <batazor111@gmail.com>
2019-10-04 16:46:45 +0300

    Update store.go

--- pkg/store/store.go
+++ pkg/store/store.go
@@ -1,12 +1,12 @@
 package store
 
 import (
-  "github.com/batazor/shortlink/pkg/api"
+  "github.com/batazor/shortlink/pkg/link"
 )
 
 type Store interface {
-  Get(id string) (api.Link, error)
-  Add(link api.Link) (api.Link, error)
-  Update(link api.Link) (api.Link, error)
+  Get(id string) (link.Link, error)
+  Add(link link.Link) (link.Link, error)
+  Update(link link.Link) (link.Link, error)
   Delete(id string) error
 }
