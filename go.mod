377cd71ff106b52c7f3767dde77c6f8511b5f2c4
v.login <v.login@s7.ru>
2019-10-11 17:25:15 +0300

    add zap logger. Close #2

--- go.mod
+++ go.mod
@@ -13,9 +13,13 @@ require (
 	github.com/golang/snappy v0.0.1 // indirect
 	github.com/lib/pq v1.2.0
 	github.com/syndtr/goleveldb v1.0.0
+	github.com/tidwall/pretty v1.0.0 // indirect
 	github.com/xdg/scram v0.0.0-20180814205039-7eeb5667e42c // indirect
 	github.com/xdg/stringprep v1.0.0 // indirect
 	go.mongodb.org/mongo-driver v1.1.2
+	go.uber.org/atomic v1.4.0 // indirect
+	go.uber.org/multierr v1.2.0 // indirect
+	go.uber.org/zap v1.10.0
 	golang.org/x/net v0.0.0-20191009170851-d66e71096ffb // indirect
 	golang.org/x/sync v0.0.0-20190911185100-cd5d95a43a6e // indirect
 	google.golang.org/grpc v1.24.0
---
+++ pkg/api/middleware/logger.go
@@ -0,0 +1,51 @@
+package middleware
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+
+	"go.uber.org/zap/zapcore"
+
+	"github.com/go-chi/chi/middleware"
+	"go.uber.org/zap"
+)
+
+type chilogger struct {
+	logZ *zap.Logger
+}
+
+// NewZapMiddleware returns a new Zap Middleware handler.
+func Logger(logger *zap.Logger) func(next http.Handler) http.Handler {
+	return chilogger{
+		logZ: logger,
+	}.middleware
+}
+
+func (c chilogger) middleware(next http.Handler) http.Handler {
+	fn := func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+		var requestID string
+		if reqID := r.Context().Value(middleware.RequestIDKey); reqID != nil {
+			requestID = reqID.(string)
+		}
+		ww := middleware.NewWrapResponseWriter(w, r.ProtoMajor)
+		next.ServeHTTP(ww, r)
+
+		latency := time.Since(start)
+
+		fields := []zapcore.Field{
+			zap.Int("status", ww.Status()),
+			zap.Duration("took", latency),
+			zap.Int64(fmt.Sprintf("measure#%s.latency"), latency.Nanoseconds()),
+			zap.String("remote", r.RemoteAddr),
+			zap.String("request", r.RequestURI),
+			zap.String("method", r.Method),
+		}
+		if requestID != "" {
+			fields = append(fields, zap.String("request-id", requestID))
+		}
+		c.logZ.Info("request completed", fields...)
+	}
+	return http.HandlerFunc(fn)
+}
