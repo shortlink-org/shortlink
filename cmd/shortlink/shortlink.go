cb838de3ff8a89dcf715a0d63ac043101251a507
v.login <v.login@s7.ru>
2019-10-15 18:19:37 +0300

    refactoring: HTTP-server take out as package from main.go

--- cmd/shortlink/shortlink.go
+++ cmd/shortlink/shortlink.go
@@ -2,70 +2,23 @@ package main
 
 import (
 	"context"
-	"fmt"
-	"github.com/batazor/shortlink/pkg/api"
-	additionalMiddleware "github.com/batazor/shortlink/pkg/api/middleware"
+	"github.com/batazor/shortlink/pkg/api/http"
 	log "github.com/batazor/shortlink/pkg/logger"
-	"github.com/go-chi/chi"
-	"github.com/go-chi/chi/middleware"
-	"github.com/go-chi/cors"
-	"github.com/go-chi/render"
 	"go.uber.org/zap"
-	"net/http"
 )
 
 func main() {
-	// Run HTTP-server
-	PORT := "7070"
-
 	// Logger
 	logger, _ := zap.NewProduction()
 	defer logger.Sync() // flushes buffer, if any
 
 	// Add context
 	ctx := context.Background()
-	ctx = log.WithLogger(ctx, logger)
-
-	r := chi.NewRouter()
-
-	// CORS
-	cors := cors.New(cors.Options{
-		AllowedOrigins:   []string{"*"},
-		AllowedMethods:   []string{"GET", "POST", "DELETE", "OPTIONS"},
-		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
-		ExposedHeaders:   []string{"Link"},
-		AllowCredentials: true,
-		MaxAge:           300,
-		//Debug:            true,
-	})
-
-	r.Use(cors.Handler)
-	r.Use(render.SetContentType(render.ContentTypeJSON))
-
-	// A good base middleware stack
-	r.Use(middleware.RequestID)
-	r.Use(middleware.RealIP)
-	r.Use(middleware.Heartbeat("/healthz"))
-	r.Use(middleware.Recoverer)
-
-	// Additional middleware
-	r.Use(additionalMiddleware.Logger(logger))
-
-	r.NotFound(NotFoundHandler)
-
-	r.Mount("/", api.Routes())
-	logger.Info(fmt.Sprintf("Run on port %s", PORT))
-
-	srv := http.Server{Addr: ":" + PORT, Handler: chi.ServerBaseContext(ctx, r)}
+	ctx = log.WithLogger(ctx, *logger)
 
 	// start HTTP-server
-	err := srv.ListenAndServe()
+	err := http.Run(ctx)
 	if err != nil {
 		logger.Panic(err.Error())
 	}
-}
-
-func NotFoundHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusNotFound)
 }
--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -5,10 +5,10 @@ import (
 	"go.uber.org/zap"
 )
 
-func WithLogger(ctx context.Context, logger *zap.Logger) context.Context {
-	return context.WithValue(ctx, "logger", &logger)
+func WithLogger(ctx context.Context, logger zap.Logger) context.Context {
+	return context.WithValue(ctx, "logger", logger)
 }
 
-func GetLogger(ctx context.Context) *zap.Logger {
-	return ctx.Value("logger").(*zap.Logger)
+func GetLogger(ctx context.Context) zap.Logger {
+	return ctx.Value("logger").(zap.Logger)
 }
