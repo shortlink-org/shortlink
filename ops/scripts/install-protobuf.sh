baf600ddcd5fa01b8292215795b617b302c7822b
Victor Login <batazor111@gmail.com>
2019-10-26 01:28:54 +0300

    fix: script installing protoc
    Signed-off-by: Victor Login <batazor111@gmail.com>

--- ops/scripts/install-protobuf.sh
+++ ops/scripts/install-protobuf.sh
@@ -6,4 +6,4 @@ basename=protoc-$PROTOBUF_VERSION-linux-x86_64.zip
 
 wget https://github.com/protocolbuffers/protobuf/releases/download/v$PROTOBUF_VERSION/$basename
 unzip $basename
-mv bin/protoc /use/bin/protoc
+mv bin/protoc /usr/bin/protoc
