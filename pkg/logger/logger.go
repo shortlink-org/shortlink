983a4fc85d4025ce6199b2584d901680c9456850
v.login <v.login@s7.ru>
2019-10-28 19:02:45 +0300

    begin work by logger pkg
    Signed-off-by: v.login <v.login@s7.ru>

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -5,6 +5,19 @@ import (
 	"go.uber.org/zap"
 )
 
+func NewLogger(config Configuration, loggerInstance int) (Logger, error) { // nolint unused
+	var log Logger
+
+	switch loggerInstance {
+	case Zap:
+		log = &zapLogger{}
+	case Logrus:
+		return &logrusLogger{}, nil
+	}
+
+	return log, nil
+}
+
 // WithLogger set logger
 func WithLogger(ctx context.Context, logger zap.Logger) context.Context { //nolint unused
 	return context.WithValue(ctx, keyLogger, logger)
