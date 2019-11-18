054ee7d13c04e44342812bbcb54f44f683a60ed5
v.login <v.login@s7.ru>
2019-11-18 16:53:36 +0300

    add test store
    Signed-off-by: v.login <v.login@s7.ru>

--- go.mod
+++ go.mod
@@ -3,11 +3,18 @@ module github.com/batazor/shortlink
 go 1.13
 
 require (
+	github.com/Azure/go-ansiterm v0.0.0-20170929234023-d6e3b3328b78 // indirect
 	github.com/Masterminds/squirrel v1.1.0
+	github.com/Microsoft/go-winio v0.4.14 // indirect
+	github.com/Nvveen/Gotty v0.0.0-20120604004816-cd527374f1e5 // indirect
+	github.com/cenkalti/backoff v2.2.1+incompatible // indirect
 	github.com/codahale/hdrhistogram v0.0.0-20161010025455-3a0bb77429bd // indirect
+	github.com/containerd/continuity v0.0.0-20190827140505-75bee3e2ccb6 // indirect
 	github.com/dgraph-io/badger v1.6.0
 	github.com/dgraph-io/badger/v2 v2.0.0 // indirect
 	github.com/dgraph-io/dgo/v2 v2.1.0
+	github.com/docker/go-connections v0.4.0 // indirect
+	github.com/docker/go-units v0.4.0 // indirect
 	github.com/go-chi/chi v4.0.2+incompatible
 	github.com/go-chi/cors v1.0.0
 	github.com/go-chi/render v1.0.1
@@ -22,7 +29,11 @@ require (
 	github.com/mattn/go-sqlite3 v1.12.0
 	github.com/onsi/ginkgo v1.10.1 // indirect
 	github.com/onsi/gomega v1.7.0 // indirect
+	github.com/opencontainers/go-digest v1.0.0-rc1 // indirect
+	github.com/opencontainers/image-spec v1.0.1 // indirect
+	github.com/opencontainers/runc v0.1.1 // indirect
 	github.com/opentracing/opentracing-go v1.1.0
+	github.com/ory/dockertest v3.3.5+incompatible
 	github.com/prometheus/client_golang v1.2.1
 	github.com/sirupsen/logrus v1.4.2
 	github.com/spf13/viper v1.3.2
---
+++ internal/store/mongo_test.go
@@ -0,0 +1,59 @@
+package store
+
+import (
+	"fmt"
+	"github.com/ory/dockertest"
+	"os"
+	"testing"
+)
+
+func TestMongo(t *testing.T) {
+	store := MongoLinkList{}
+
+	// uses a sensible default on windows (tcp/http) and linux/osx (socket)
+	pool, err := dockertest.NewPool("")
+	if err != nil {
+		t.Fatalf("Could not connect to docker: %s", err)
+	}
+
+	// pulls an image, creates a container based on it and runs it
+	resource, err := pool.Run("mongo", "latest", nil)
+	if err != nil {
+		t.Fatalf("Could not start resource: %s", err)
+	}
+
+	// exponential backoff-retry, because the application in the container might not be ready to accept connections yet
+	if err := pool.Retry(func() error {
+		var err error
+
+		err = os.Setenv("STORE_MONGODB_URI", fmt.Sprintf("mongodb://localhost:%s", resource.GetPort("27017/tcp")))
+		if err != nil {
+			t.Fatalf("Cannot set ENV: %s", err)
+		}
+
+		err = store.Init()
+		if err != nil {
+			return err
+		}
+
+		return nil
+	}); err != nil {
+		t.Fatalf("Could not connect to docker: %s", err)
+	}
+
+	t.Run("Create", func(t *testing.T) {
+		link, err := store.Add(addLink)
+		if err != nil {
+			t.Error(err)
+		}
+
+		if link.Hash != getLink.Hash {
+			t.Errorf("Assert hash - %s; Get %s hash", getLink.Hash, link.Hash)
+		}
+	})
+
+	// You can't defer this because os.Exit doesn't care for defer
+	if err := pool.Purge(resource); err != nil {
+		t.Fatalf("Could not purge resource: %s", err)
+	}
+}
