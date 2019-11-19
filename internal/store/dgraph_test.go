76fca616e353ece46e8bc3798d8310c235cd3abb
v.login <v.login@s7.ru>
2019-11-19 17:27:38 +0300

    improve test - store
    Signed-off-by: v.login <v.login@s7.ru>

--- internal/store/dgraph_test.go
+++ internal/store/dgraph_test.go
@@ -86,6 +86,42 @@ func TestDgraph(t *testing.T) {
 		}
 	})
 
+	t.Run("Get", func(t *testing.T) {
+		link, err := store.Get(getLink.Hash)
+		if err != nil {
+			t.Error(err)
+		}
+
+		if link.Hash != getLink.Hash {
+			t.Errorf("Assert hash - %s; Get %s hash", getLink.Hash, link.Hash)
+		}
+	})
+
+	t.Run("Get list", func(t *testing.T) {
+		links, err := store.List()
+		if err != nil {
+			t.Error(err)
+		}
+
+		if len(links) == 1 {
+			t.Errorf("Assert 1 links; Get %d link(s)", len(links))
+		}
+	})
+
+	t.Run("Delete", func(t *testing.T) {
+		err := store.Delete(getLink.Hash)
+		if err != nil {
+			t.Error(err)
+		}
+	})
+
+	t.Run("Close", func(t *testing.T) {
+		err := store.Close()
+		if err != nil {
+			t.Error(err)
+		}
+	})
+
 	// When you're done, kill and remove the container
 	if err := pool.Purge(ALPHA); err != nil {
 		t.Errorf("Could not purge resource: %s", err)
