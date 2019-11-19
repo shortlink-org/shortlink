5a0821f44741d51d0c719809948f872c974d65a9
v.login <v.login@s7.ru>
2019-11-19 17:27:38 +0300

    improve test - store
    Signed-off-by: v.login <v.login@s7.ru>

--- internal/store/badger_test.go
+++ internal/store/badger_test.go
@@ -22,4 +22,40 @@ func TestBadger(t *testing.T) {
 			t.Errorf("Assert hash - %s; Get %s hash", getLink.Hash, link.Hash)
 		}
 	})
+
+	t.Run("Get", func(t *testing.T) {
+		link, err := store.Get(getLink.Hash)
+		if err != nil {
+			t.Error(err)
+		}
+
+		if link.Hash != getLink.Hash {
+			t.Errorf("Assert hash - %s; Get %s hash", getLink.Hash, link.Hash)
+		}
+	})
+
+	t.Run("Get list", func(t *testing.T) {
+		links, err := store.List()
+		if err != nil {
+			t.Error(err)
+		}
+
+		if len(links) == 1 {
+			t.Errorf("Assert 1 links; Get %d link(s)", len(links))
+		}
+	})
+
+	t.Run("Delete", func(t *testing.T) {
+		err := store.Delete(getLink.Hash)
+		if err != nil {
+			t.Error(err)
+		}
+	})
+
+	t.Run("Close", func(t *testing.T) {
+		err := store.Close()
+		if err != nil {
+			t.Error(err)
+		}
+	})
 }
