21ca1a186c4de475b1aa0996b3189e96e4486af3
v.login <v.login@s7.ru>
2019-11-21 17:06:08 +0300

    grpc-gateway add traicing
    Signed-off-by: v.login <v.login@s7.ru>

--- pkg/api/grpc-web/server.go
+++ pkg/api/grpc-web/server.go
@@ -3,14 +3,18 @@ package grpcweb
 import (
 	"context"
 	"fmt"
-	"github.com/batazor/shortlink/internal/freeport"
-	"github.com/batazor/shortlink/pkg/api"
+	"github.com/batazor/shortlink/internal/traicing"
+	"github.com/opentracing/opentracing-go/ext"
 	"net"
 	"net/http"
 
+	"github.com/batazor/shortlink/internal/freeport"
 	"github.com/batazor/shortlink/internal/logger"
 	"github.com/batazor/shortlink/internal/store"
+	"github.com/batazor/shortlink/pkg/api"
+	"github.com/grpc-ecosystem/go-grpc-middleware/tracing/opentracing"
 	"github.com/grpc-ecosystem/grpc-gateway/runtime"
+	"github.com/opentracing/opentracing-go"
 	"google.golang.org/grpc"
 )
 
@@ -19,6 +23,8 @@ type API struct { // nolint unused
 	store store.DB
 	ctx   context.Context
 }
+
+var grpcGatewayTag = opentracing.Tag{Key: string(ext.Component), Value: "grpc-gateway"}
 
 // Run HTTP-server
 func (api *API) Run(ctx context.Context, db store.DB, config api.Config) error {
@@ -31,6 +37,7 @@ func (api *API) Run(ctx context.Context, db store.DB, config api.Config) error {
 		return err
 	}
 
+	// Get logger
 	log := logger.GetLogger(ctx)
 	log.Info(fmt.Sprintf("Run gRPC-GateWay on localhost:%d", port))
 
@@ -41,18 +48,29 @@ func (api *API) Run(ctx context.Context, db store.DB, config api.Config) error {
 		}
 	}()
 
+	// Get tracer
+	tracer := traicing.GetTraicer(ctx)
+
 	// Register gRPC server endpoint
 	// Note: Make sure the gRPC server is running properly and accessible
 	gw := runtime.NewServeMux(
 		runtime.WithProtoErrorHandler(runtime.DefaultHTTPProtoErrorHandler),
 	)
-	opts := []grpc.DialOption{grpc.WithInsecure()}
+
+	opts := []grpc.DialOption{
+		grpc.WithInsecure(),
+		grpc.WithUnaryInterceptor(
+			grpc_opentracing.UnaryClientInterceptor(
+				grpc_opentracing.WithTracer(tracer),
+			),
+		),
+	}
 	err = RegisterLinkHandlerFromEndpoint(ctx, gw, fmt.Sprintf("localhost:%d", port), opts)
 	if err != nil {
 		return err
 	}
 
-	srv := http.Server{Addr: fmt.Sprintf(":%d", config.Port), Handler: gw}
+	srv := http.Server{Addr: fmt.Sprintf(":%d", config.Port), Handler: api.tracingWrapper(gw)}
 
 	// Start HTTP server (and proxy calls to gRPC server endpoint)
 	err = srv.ListenAndServe()
@@ -70,4 +88,23 @@ func (api *API) runGRPC(port int) error {
 	RegisterLinkServer(grpcServer, api)
 	err = grpcServer.Serve(lis)
 	return err
+}
+
+func (api *API) tracingWrapper(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		parentSpanContext, err := opentracing.GlobalTracer().Extract(
+			opentracing.HTTPHeaders,
+			opentracing.HTTPHeadersCarrier(r.Header))
+		if err == nil || err == opentracing.ErrSpanContextNotFound {
+			serverSpan := opentracing.GlobalTracer().StartSpan(
+				"ServeHTTP",
+				// this is magical, it attaches the new span to the parent parentSpanContext, and creates an unparented one if empty.
+				ext.RPCServerOption(parentSpanContext),
+				grpcGatewayTag,
+			)
+			r = r.WithContext(opentracing.ContextWithSpan(r.Context(), serverSpan))
+			defer serverSpan.Finish()
+		}
+		h.ServeHTTP(w, r)
+	})
 }
