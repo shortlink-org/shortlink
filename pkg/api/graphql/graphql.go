72f679df66cdb0a6d8cf0ced990495ae7863a0fc
v.login <v.login@s7.ru>
2019-11-28 12:37:55 +0300

    close #149
    Signed-off-by: v.login <v.login@s7.ru>

--- pkg/api/graphql/graphql.go
+++ pkg/api/graphql/graphql.go
@@ -1,17 +1,19 @@
 package graphql
 
 import (
+	"bytes"
 	"context"
 	"fmt"
 	"net/http"
+	"os"
 
 	"github.com/batazor/shortlink/internal/logger"
 	"github.com/batazor/shortlink/internal/store"
 	"github.com/batazor/shortlink/pkg/api"
 	"github.com/batazor/shortlink/pkg/api/graphql/resolver"
-	"github.com/batazor/shortlink/pkg/api/graphql/schema"
 	"github.com/graph-gophers/graphql-go"
 	"github.com/graph-gophers/graphql-go/relay"
+	"github.com/markbates/pkger"
 )
 
 // API ...
@@ -22,7 +24,41 @@ type API struct { // nolint unused
 
 // GetHandler ...
 func (api *API) GetHandler() *relay.Handler {
-	s := graphql.MustParseSchema(schema.GetRootSchema(), &resolver.Resolver{Store: api.store})
+	buf := bytes.Buffer{}
+
+	err := pkger.Walk("/pkg/api/graphql/schema", func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		if !info.IsDir() {
+			file, _ := pkger.Open(path)
+			f := make([]byte, info.Size())
+			_, err := file.Read(f)
+
+			// Add a newline if the file does not end in a newline.
+			if len(f) > 0 && f[len(f)-1] != '\n' {
+				if err := buf.WriteByte('\n'); err != nil {
+					panic(err)
+				}
+			}
+
+			if err != nil {
+				panic(err)
+			}
+
+			if _, err := buf.Write(f); err != nil {
+				panic(err)
+			}
+		}
+
+		return nil
+	})
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	s := graphql.MustParseSchema(buf.String(), &resolver.Resolver{Store: api.store})
 	handler := relay.Handler{Schema: s}
 
 	return &handler
