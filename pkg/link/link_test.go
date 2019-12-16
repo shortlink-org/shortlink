436cd706638112ad9759d09d7fe3dff90fe79b01
v.login <v.login@s7.ru>
2019-12-16 17:23:54 +0300

    improve test link pkg
    Signed-off-by: v.login <v.login@s7.ru>

--- pkg/link/link_test.go
+++ pkg/link/link_test.go
@@ -4,12 +4,30 @@ import "testing"
 
 func TestNewURL(t *testing.T) {
 	URL := "http://test.com"
-	newUrl, err := NewURL("http://test.com")
-	if err != nil {
-		t.Errorf("Assert Success. Got: %s", err)
-	}
 
-	if newUrl.Url != URL {
-		t.Errorf("Assert %s. Got: %s", URL, newUrl.Url)
-	}
+	t.Run("create new", func(t *testing.T) {
+		newUrl, err := NewURL(URL)
+
+		if err != nil {
+			t.Errorf("Assert nil. Got: %s", err)
+		}
+
+		if newUrl.Url != URL {
+			t.Errorf("Assert %s. Got: %s", URL, newUrl.Url)
+		}
+	})
+
+	t.Run("create hash", func(t *testing.T) {
+		newUrl, err := NewURL(URL)
+
+		if err != nil {
+			t.Errorf("Assert nil. Got: %s", err)
+		}
+
+		success := "99699cbfa9614160a94114f527f5501fd97edeaa767db24bb8581789e18a9c1f0f671ee525a9404abc4a8d015315f773dd214175a9c50ac6cda1d934f75fc1e8"
+		response := newUrl.CreateHash([]byte("hello world"), []byte("solt"))
+		if success != response {
+			t.Errorf("Assert %s. Got: %s", success, response)
+		}
+	})
 }
