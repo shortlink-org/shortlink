53d0a3c70b6e295e3c50d7e71c6f183677420083
v.login <v.login@s7.ru>
2019-12-20 13:38:28 +0300

    store: update mock for support create_at, update_at
    Signed-off-by: v.login <v.login@s7.ru>

--- internal/store/mock/mock.go
+++ internal/store/mock/mock.go
@@ -1,17 +1,25 @@
 package mock
 
-import "github.com/batazor/shortlink/pkg/link"
+import (
+	"github.com/batazor/shortlink/pkg/link"
+	"github.com/golang/protobuf/ptypes"
+)
 
 var (
-	AddLink = link.Link{
-		Url:      "https://example.com",
-		Hash:     "",
-		Describe: "example link",
+	timestamp = ptypes.TimestampNow()
+	AddLink   = link.Link{
+		Url:       "https://example.com",
+		Hash:      "",
+		Describe:  "example link",
+		CreatedAt: timestamp,
+		UpdatedAt: timestamp,
 	}
 
 	GetLink = link.Link{
-		Url:      "https://example.com",
-		Hash:     "5888cab",
-		Describe: "example link",
+		Url:       "https://example.com",
+		Hash:      "5888cab",
+		Describe:  "example link",
+		CreatedAt: timestamp,
+		UpdatedAt: timestamp,
 	}
 )
