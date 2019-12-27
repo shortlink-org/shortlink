6056e06b9ea35fbb221af0a7854d15bb2adc2c2f
v.login <v.login@s7.ru>
2019-12-27 18:22:09 +0300

    ram: use sync.Map
    Signed-off-by: v.login <v.login@s7.ru>

--- internal/store/ram/ram.go
+++ internal/store/ram/ram.go
@@ -9,17 +9,13 @@ import (
 )
 
 // RAMLinkList implementation of store interface
-type RAMLinkList struct { // nolint unused
-	links map[string]link.Link
-	mu    sync.RWMutex
+type RAMLinkList struct {
+	// sync.Map solver problem with cache contention
+	links sync.Map
 }
 
 // Init ...
 func (ram *RAMLinkList) Init() error { // nolint unparam
-	ram.mu.Lock()
-	ram.links = make(map[string]link.Link)
-	ram.mu.Unlock()
-
 	return nil
 }
 
@@ -34,34 +30,25 @@ func (ram *RAMLinkList) migrate() error { // nolint unused
 }
 
 // Get ...
-func (ram *RAMLinkList) Get(id string) (*link.Link, error) {
-	ram.mu.Lock()
-	response := ram.links[id]
-	ram.mu.Unlock()
-
-	if response.Url == "" {
-		return nil, &link.NotFoundError{Link: link.Link{Url: id}, Err: fmt.Errorf("Not found id: %s", id)}
+func (ram *RAMLinkList) Get(hash string) (*link.Link, error) {
+	response, ok := ram.links.Load(hash)
+	if !ok {
+		return nil, &link.NotFoundError{Link: link.Link{Url: hash}, Err: fmt.Errorf("Not found id: %s", hash)}
 	}
 
-	return &response, nil
+	link := response.(link.Link)
+	return &link, nil
 }
 
 // List ...
 func (ram *RAMLinkList) List(filter *query.Filter) ([]*link.Link, error) { // nolint unused
 	links := []*link.Link{}
 
-	ram.mu.Lock()
-	// copy map by assigning elements to new map
-	for key := range ram.links {
-		links = append(links, &link.Link{
-			Url:       ram.links[key].Url,
-			Hash:      ram.links[key].Hash,
-			Describe:  ram.links[key].Describe,
-			CreatedAt: ram.links[key].CreatedAt,
-			UpdatedAt: ram.links[key].UpdatedAt,
-		})
-	}
-	ram.mu.Unlock()
+	ram.links.Range(func(key interface{}, value interface{}) bool {
+		link := value.(link.Link)
+		links = append(links, &link)
+		return true
+	})
 
 	return links, nil
 }
@@ -73,9 +60,7 @@ func (ram *RAMLinkList) Add(source link.Link) (*link.Link, error) { // nolint un
 		return nil, err
 	}
 
-	ram.mu.RLock()
-	ram.links[data.Hash] = data
-	ram.mu.RUnlock()
+	ram.links.Store(data.Hash, data)
 
 	return &data, nil
 }
@@ -86,10 +71,7 @@ func (ram *RAMLinkList) Update(data link.Link) (*link.Link, error) {
 }
 
 // Delete ...
-func (ram *RAMLinkList) Delete(id string) error { // nolint unused
-	ram.mu.Lock()
-	delete(ram.links, id)
-	ram.mu.Unlock()
-
+func (ram *RAMLinkList) Delete(hash string) error { // nolint unused
+	ram.links.Delete(hash)
 	return nil
 }
