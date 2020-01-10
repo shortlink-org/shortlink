a6b7030fc8a2e0ae6dea40fe6f1c83a9666a6360
v.login <v.login@s7.ru>
2020-01-10 12:17:49 +0300

    upd postgres config
    Signed-off-by: v.login <v.login@s7.ru>

--- initialState/init-postgres.sql
+++ initialState/init-postgres.sql
@@ -1,21 +1,43 @@
-create table links
+CREATE TABLE links
 (
     id       serial       not null
-        constraint links_pk
-            primary key,
+             constraint links_pk
+             primary key,
     url      varchar(255) not null,
     hash     varchar(255) not null,
     describe text
 );
 
-comment on table links is 'Link list';
+COMMENT ON TABLE links IS 'Link list';
 
-alter table links
-    owner to shortlink;
+ALTER TABLE links
+    OWNER TO shortlink;
 
-create unique index links_id_uindex
-    on links (id);
+CREATE UNIQUE INDEX links_id_uindex
+    ON links (id);
 
-create unique index links_hash_uindex
-    on links (hash);
+CREATE UNIQUE INDEX links_hash_uindex
+    ON links (hash);
 
+-- INCLUDE-index
+-- as example: SELECT id, url, hash FROM links WHERE id = 10;
+CREATE UNIQUE INDEX links_list ON links USING btree (hash) INCLUDE (url);
+
+-- TRANSACTION ---------------------------------------------------------------------------------------------------------
+BEGIN
+    ISOLATION LEVEL READ COMMITTED;
+
+SAVEPOINT tx_create_default_links;
+
+INSERT INTO links(url, hash, describe)
+    VALUES ('https://batazor.ru', 'myHash1', 'My personal website');
+
+INSERT INTO links(url, hash, describe)
+VALUES ('https://github.com/batazor', 'myHash2', 'My accout of github');
+
+INSERT INTO links(url, hash, describe)
+VALUES ('https://vk.com/batazor', 'myHash3', 'My page on vk.com');
+
+-- ROLLBACK TO tx_create_default_links;
+
+COMMIT;
