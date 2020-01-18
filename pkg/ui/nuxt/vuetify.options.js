7bc715b8d68adbe8d18d0841502fcd7c5ee4e2f6
Victor Login <batazor111@gmail.com>
2020-01-18 07:23:58 +0300

    update UI

---
+++ pkg/ui/nuxt/vuetify.options.js
@@ -0,0 +1,29 @@
+export default {
+  breakpoint: {},
+  icons: {},
+  lang: {},
+  rtl: false,
+  theme: {
+    default: 'light',
+    themes: {
+      light: {
+        primary: '#1976D2',
+        secondary: '#424242',
+        accent: '#82B1FF',
+        error: '#FF5252',
+        info: '#2196F3',
+        success: '#4CAF50',
+        warning: '#FB8C00',
+      },
+      dark: {
+        primary: '#2196F3',
+        secondary: '#424242',
+        accent: '#FF4081',
+        error: '#FF5252',
+        info: '#2196F3',
+        success: '#4CAF50',
+        warning: '#FB8C00',
+      },
+    },
+  },
+}
