9325bf3c89b12486ea4eb40af170c86e89a7e726
Victor Login <batazor111@gmail.com>
2020-02-05 02:52:54 +0300

    init nextjs
    Signed-off-by: Victor Login <batazor111@gmail.com>

---
+++ pkg/ui/next/pages/about.js
@@ -0,0 +1,7 @@
+import Layout from '../components/MyLayout.js';
+
+const aboutPageContent = <p>This is the about page</p>;
+
+export default function About() {
+  return <Layout content={aboutPageContent} />;
+}
