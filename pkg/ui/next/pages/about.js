90d3e62634c016553e67935b1036a42ce0673ad5
Login Victor <batazor111@gmail.com>
2020-04-12 03:14:08 +0300

    init sentry
    Signed-off-by: Login Victor <batazor111@gmail.com>

--- pkg/ui/next/pages/about.js
+++ pkg/ui/next/pages/about.js
@@ -3,5 +3,7 @@ import Layout from '../components/MyLayout.js';
 const aboutPageContent = <p>This is the about page</p>;
 
 export default function About() {
+  return new Error("Something broke")
+
   return <Layout content={aboutPageContent} />;
 }
