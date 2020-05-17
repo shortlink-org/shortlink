732fc33f8f98abc1007d1edbc2539b8408c0dc11
Login Victor <batazor111@gmail.com>
2020-05-17 13:40:29 +0300

    upd
    Signed-off-by: Login Victor <batazor111@gmail.com>

---
+++ pkg/ui/next/components/AddURL.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import TextField from '@material-ui/core/TextField'
+
+export default function AddURL() {
+  return (
+    <form autoComplete="off">
+      <TextField label="Add URL" />
+    </form>
+  );
+}
