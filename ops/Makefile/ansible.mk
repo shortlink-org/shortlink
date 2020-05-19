217c7174428f9b4fbeef89e9d008e78e8c0ed412
Login Victor <batazor111@gmail.com>
2020-05-19 23:17:03 +0300

    docker: Set up automated test files
    Signed-off-by: Login Victor <batazor111@gmail.com>

--- ops/Makefile/ansible.mk
+++ ops/Makefile/ansible.mk
@@ -7,3 +7,8 @@ ansible-dep: ## Install ansible dep
 ansible-up: ## Apply ansible playbook
 	@ansible-playbook ops/ansible/playbooks/playbook.yml -i ops/ansible/hosts
 
+ansible-do: ## Test
+	@ansible-playbook ops/ansible/playbooks/playbook.yml -v -i ops/ansible/hosts \
+ 		--tags "test" \
+ 		--extra-vars "rest_server=http://google.com username=admin password=admin"
+
