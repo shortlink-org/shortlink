efb5865114708c8f39a01e039c451bf6e8b71130
Login Victor <batazor111@gmail.com>
2020-05-22 14:47:10 +0300

    enable istio

--- ops/Makefile/istio.mk
+++ ops/Makefile/istio.mk
@@ -1,4 +1,7 @@
 # ISTIO TASKS ==========================================================================================================
 istio-up: ## Run istio
-	@istioctl manifest apply --set profile=demo
+	@istioctl manifest apply \
+		--set profile=demo \
+		--set gateways.istio-ingressgateway.type=NodePort \
+		--set gateways.istio-egressgateway.type=NodePort
 	@kubectl label namespace default istio-injection=enabled
--- ops/Makefile/k8s.mk
+++ ops/Makefile/k8s.mk
@@ -1,9 +1,10 @@
 # KUBERNETES TASKS =====================================================================================================
-PATH_TO_SHORTLINK_CHART := ops/Helm/shortlink-ui
 PATH_TO_COMMON_CHART := ops/Helm/common
 
 SHORTLINK_NAMESPACE := shortlink
 SHORTLINK_HELM_API := ops/Helm/shortlink-api
+SHORTLINK_HELM_UI := ops/Helm/shortlink-ui
+SHORTLINK_HELM_INGRESS := ops/Helm/ingress
 
 helm-init: ## helm init
 	@echo "add custom repo for helm"
@@ -11,19 +12,10 @@ helm-init: ## helm init
 	@helm repo add istio https://storage.googleapis.com/istio-release/releases/1.5.4/charts/
 
 helm-lint: ## Check Helm chart
-	@helm lint ${PATH_TO_SHORTLINK_CHART}
 	@helm lint ${PATH_TO_COMMON_CHART}
 	@helm lint ${SHORTLINK_HELM_API}
-
-helm-deploy: ## Deploy Helm chart to default kube-context and default namespace
-	@echo helm install/update ${PROJECT_NAME}
-	@helm upgrade ${PROJECT_NAME} ${PATH_TO_SHORTLINK_CHART} \
-		--install \
-		--force \
-		--wait
-
-helm-clean: ## Clean artifact from K8S
-	@helm del ${PROJECT_NAME}
+	@helm lint ${SHORTLINK_HELM_UI}
+	@helm lint ${SHORTLINK_HELM_INGRESS}
 
 helm-common: ## run common service for
 	@make helm-init
@@ -34,9 +26,28 @@ helm-common: ## run common service for
 		--wait
 
 helm-shortlink-up: ## run shortlink in k8s by Helm
+	@echo helm install/update ${PROJECT_NAME}
+
 	@helm upgrade api ${SHORTLINK_HELM_API} \
 		--install \
 		--force \
 		--namespace=${SHORTLINK_NAMESPACE} \
 		--create-namespace=true \
 		--wait
+
+	@helm upgrade ui ${SHORTLINK_HELM_UI} \
+		--install \
+		--force \
+		--namespace=${SHORTLINK_NAMESPACE} \
+		--wait \
+		--set serviceAccount.create=false
+
+	@helm upgrade ingress ${SHORTLINK_HELM_INGRESS} \
+		--install \
+		--force \
+		--namespace=${SHORTLINK_NAMESPACE} \
+		--wait
+
+helm-shortlink-down: ## Clean artifact from K8S
+	@helm -n ${SHORTLINK_NAMESPACE} del api
+	@helm -n ${SHORTLINK_NAMESPACE} del ui
