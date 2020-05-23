60eacf306a5b56af9f234126b50667cf7b303773
Login Victor <batazor111@gmail.com>
2020-05-23 23:08:25 +0300

    add terraform

---
+++ ops/Makefile/terraform.mk
@@ -0,0 +1,9 @@
+# TERRAFORM TASKS ======================================================================================================
+TF_DATA_DIR := ops/terraform
+
+terraform-init: ## Terraform init
+	@terraform init ${TF_DATA_DIR}
+	@terraform plan ${TF_DATA_DIR}
+
+terraform-up: ## Terraform up
+	@terraform apply -auto-approve ${TF_DATA_DIR}
---
+++ ops/terraform/main.tf
@@ -0,0 +1,26 @@
+provider "postgresql" {
+  alias           = "pg"
+  host            = "127.0.0.1"
+  port            = 5432
+  database        = var.psql-database
+  username        = var.psql-user
+  password        = var.psql-password
+  sslmode         = "disable"
+  connect_timeout = 15
+}
+
+provider "kubernetes" {
+  config_context_auth_info = "minikube"
+  config_context_cluster   = "minikube"
+}
+
+resource "postgresql_database" "my_db" {
+  provider = "postgresql.pg"
+  name     = "my_db"
+}
+
+resource "kubernetes_namespace" "example" {
+  metadata {
+    name = "my-first-namespace"
+  }
+}
