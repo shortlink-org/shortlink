496b7e121214537d64adcda49166d45d0da68ef5
Login Victor <batazor111@gmail.com>
2020-05-24 17:57:04 +0300

    monitoirng: add telegram notify

--- ops/Makefile/prometheus.mk
+++ ops/Makefile/prometheus.mk
@@ -4,9 +4,13 @@ export PROMETHEUS_SLACK := https://hooks.slack.com/services/TSP6U57H9/B013RLLCSR
 
 # docs: https://www.robustperception.io/sending-email-with-the-alertmanager-via-gmail
 export PROMETHEUS_SLACK_CHANNEL := \#monitoring
-export PROMETHEUS_MAIL_ACCOUNT := user@gmail.com
-export PROMETHEUS_MAIL_HOST := smtp.gmail.com:587
+export PROMETHEUS_MAIL_ACCOUNT  := user@gmail.com
+export PROMETHEUS_MAIL_HOST     := smtp.gmail.com:587
 export PROMETHEUS_MAIL_PASSWORD := you_auth_token
+
+# docs: https://github.com/metalmatze/alertmanager-bot
+export PROMETHEUS_TELEGRAM_API   := 'WFhYWFhYWA==' # in base64
+export PROMETHEUS_TELEGRAM_ADMIN := 'MTIzNA==' # in base64
 
 helm-prometheus-up:
 	@helm repo add stable https://kubernetes-charts.storage.googleapis.com
@@ -24,6 +28,15 @@ helm-prometheus-up:
         --create-namespace=true \
 		--wait \
 		-f /tmp/prometheus-operator.values.yaml
+
+prometheus-telegram-alert:
+	@envsubst < ops/Helm/telegram-alert-bot.yaml > /tmp/telegram-alert-bot.yaml
+	@kubectl apply -n ${PROMETHEUS_NAMESPACE} \
+		-f /tmp/telegram-alert-bot.yaml
+
+prometheus-telegram-alert-sown:
+	@kubectl delete -n ${PROMETHEUS_NAMESPACE} \
+		-f /tmp/telegram-alert-bot.yaml
 
 helm-prometheus-down:
 	@helm --namespace=${PROMETHEUS_NAMESPACE} delete prometheus
