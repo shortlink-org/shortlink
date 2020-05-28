21553b443da928e87d35599f3cafe1adace4db9a
Login Victor <batazor111@gmail.com>
2020-05-28 19:46:15 +0300

    refactoring

--- ops/Makefile/kubeadm.mk
+++ ops/Makefile/kubeadm.mk
@@ -2,6 +2,6 @@
 
 vagrant-up:
 	#kubeadm init --apiserver-cert-extra-sans=10.0.7.101 --pod-network-cidr=10.2.0.0/16 --apiserver-advertise-address=0.0.0.0 --kubernetes-version=v1.14.0
-	@ansible-playbook ops/ansible/application/playbooks/playbook.yml -v -i ops/ansible/hosts \
+	@ansible-playbook ops/ansible/application/playbooks/playbook.yml -v -i ops/ansible/application.host \
  		--vault-password-file ops/ansible/vault-password.txt \
  		--tags "test"
