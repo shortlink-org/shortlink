93929fb9bc88430e610ca93125cdca0126d2437d
Login Victor <batazor111@gmail.com>
2020-06-16 01:51:31 +0300

    bot: use notify package

--- internal/bot/bot.go
+++ internal/bot/bot.go
@@ -7,43 +7,50 @@ import (
 	"github.com/batazor/shortlink/internal/bot/slack"
 	"github.com/batazor/shortlink/internal/bot/smtp"
 	"github.com/batazor/shortlink/internal/bot/telegram"
+	bot_type "github.com/batazor/shortlink/internal/bot/type"
 	"github.com/batazor/shortlink/internal/notify"
 	"github.com/batazor/shortlink/pkg/link"
 )
 
 func (b *Bot) Use(ctx context.Context) { // nolint unused
 	// Subscribe to Event
-	notify.Subscribe(METHOD_NEW_LINK, b)
+	notify.Subscribe(bot_type.METHOD_NEW_LINK, b)
 
 	// Init slack bot
-	slackBot := &slack.Bot{}
-	err := slackBot.Init()
-	if err != nil {
-		return
-	}
-	b.services = append(b.services, slackBot)
+	go func() {
+		var slackBot Service
+		slackBot = &slack.Bot{}
+		err := slackBot.Init()
+		if err != nil {
+			return
+		}
+	}()
 
 	// Init telegram bot
-	telegramBot := &telegram.Bot{}
-	err = telegramBot.Init()
-	if err != nil {
-		return
-	}
-	b.services = append(b.services, telegramBot)
+	go func() {
+		var telegramBot Service
+		telegramBot = &telegram.Bot{}
+		err := telegramBot.Init()
+		if err != nil {
+			return
+		}
+	}()
 
 	// Init SMTP bot
-	smtpBot := &smtp.Bot{}
-	err = smtpBot.Init()
-	if err != nil {
-		return
-	}
-	b.services = append(b.services, smtpBot)
+	go func() {
+		var smtpBot Service
+		smtpBot = &smtp.Bot{}
+		err := smtpBot.Init()
+		if err != nil {
+			return
+		}
+	}()
 }
 
 // Notify ...
 func (b *Bot) Notify(ctx context.Context, event int, payload interface{}) notify.Response { // nolint unused
 	switch event {
-	case METHOD_NEW_LINK:
+	case bot_type.METHOD_NEW_LINK:
 		if addLink, ok := payload.(*link.Link); ok {
 			b.Send(ctx, addLink)
 		}
@@ -55,11 +62,5 @@ func (b *Bot) Notify(ctx context.Context, event int, payload interface{}) notify
 func (b *Bot) Send(ctx context.Context, link *link.Link) {
 	payload := fmt.Sprintf("LINK: %s", link.Url)
 
-	// Send message
-	for _, service := range b.services {
-		err := service.Send(payload)
-		if err != nil {
-			continue
-		}
-	}
+	notify.Publish(ctx, bot_type.METHOD_SEND_NEW_LINK, payload, nil, "")
 }
--- internal/bot/type.go
+++ internal/bot/type.go
@@ -4,18 +4,15 @@ import (
 	"github.com/batazor/shortlink/internal/notify"
 )
 
-const (
-	METHOD_NEW_LINK = 10 + iota // nolint unused
-)
-
 type Bot struct {
 	// system event
 	notify.Subscriber // Observer interface for subscribe on system event
-
-	services []Service
 }
 
 type Service interface {
+	// system event
+	notify.Subscriber // Observer interface for subscribe on system event
+
 	Init() error
 	Send(message string) error
 }
