97904bb024d0afd3995ab4ee3d504518b34953bb
Login Victor <batazor111@gmail.com>
2020-07-27 04:36:30 +0300

    first version
    Signed-off-by: Login Victor <batazor111@gmail.com>

---
+++ internal/store/options/type.go
@@ -0,0 +1,6 @@
+package options
+
+const (
+	MODE_SINGLE_WRITE = iota
+	MODE_BATCH_WRITE
+)
---
+++ internal/store/ram/ram_benchmark_test.go
@@ -0,0 +1,51 @@
+package ram
+
+import (
+	"context"
+	"os"
+	"strconv"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/batazor/shortlink/internal/store/mock"
+	"github.com/batazor/shortlink/internal/store/options"
+)
+
+func BenchmarkRAM(b *testing.B) {
+	store := RAMLinkList{}
+
+	ctx := context.Background()
+
+	b.Run("Create [single]", func(b *testing.B) {
+		// create a store
+		err := store.Init(ctx)
+		assert.Nil(b, err)
+
+		data := mock.AddLink
+
+		for i := 0; i < b.N; i++ {
+			data.Url = data.Url + "/" + string(i)
+			_, err := store.Add(ctx, mock.AddLink)
+			assert.Nil(b, err)
+		}
+	})
+
+	b.Run("Create [batch]", func(b *testing.B) {
+		// Set config
+		err := os.Setenv("STORE_MODE_WRITE", strconv.Itoa(options.MODE_BATCH_WRITE))
+		assert.Nil(b, err, "Cannot set ENV")
+
+		// create a store
+		err = store.Init(ctx)
+		assert.Nil(b, err)
+
+		data := mock.AddLink
+
+		for i := 0; i < b.N; i++ {
+			data.Url = data.Url + "/" + string(i)
+			_, err := store.Add(ctx, mock.AddLink)
+			assert.Nil(b, err)
+		}
+	})
+}
