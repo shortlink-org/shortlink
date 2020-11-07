4569521e933c69f79276e7e8562a0dfd129632f6
Login Victor <batazor111@gmail.com>
2020-11-07 03:18:21 +0300

    csi: refactoring
    Signed-off-by: Login Victor <batazor111@gmail.com>

--- pkg/csi/driver.go
+++ pkg/csi/driver.go
@@ -1,3 +1,15 @@
+/*
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package csi_driver
 
 import (
@@ -7,7 +19,6 @@ import (
 	"sync"
 
 	"github.com/container-storage-interface/spec/lib/go/csi"
-	"github.com/golang/glog"
 	"google.golang.org/grpc"
 
 	"github.com/batazor/shortlink/internal/logger"
@@ -73,8 +84,8 @@ func NewDriver(log logger.Logger, driverName, nodeID, endpoint string, maxVolume
 		return nil, fmt.Errorf("failed to create dataRoot: %v", err)
 	}
 
-	glog.Infof("Driver: %v ", driverName)
-	glog.Infof("Version: %s", vendorVersion)
+	log.Info(fmt.Sprintf("Driver: %v ", driverName))
+	log.Info(fmt.Sprintf("Version: %s", vendorVersion))
 
 	return &driver{
 		name:              driverName,
