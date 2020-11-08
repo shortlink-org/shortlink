58b40ac6e7c609e4dec2e7a3840f82900150c85e
Login Victor <batazor111@gmail.com>
2020-11-09 01:51:48 +0300

    ansible: improve
    Signed-off-by: Login Victor <batazor111@gmail.com>

--- ops/Makefile/ansible.mk
+++ ops/Makefile/ansible.mk
@@ -1,5 +1,6 @@
 # ANSIBLE TASKS ========================================================================================================
 export ANSIBLE_CONFIG=ops/ansible/ansible.cfg
+export ANSIBLE_ASK_SUDO_PASS=true
 
 ansible-dep: ## Install ansible dep
 	@ansible-galaxy install -r ops/ansible/requirements.yml
@@ -11,7 +12,7 @@ ansible-locale: ## Install locale tool
 		--tags="localhost" \
 		--limit="localhost" \
 		--vault-password-file ops/ansible/vault-password.txt \
-		ops/ansible/playbooks/playbook.yml -kK
+		ops/ansible/playbooks/playbook.yml
 
 ansible-up: ## Apply ansible playbook
 	@ansible-playbook ops/ansible/playbooks/playbook.yml -i ops/ansible/hosts.ini \
