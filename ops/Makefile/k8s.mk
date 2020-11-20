1c43fbcab0a7df51d47e83191e615e85b2a9352d
Login Victor <batazor111@gmail.com>
2020-11-20 17:16:56 +0300

    gitlab-ci: fix helm check
    Signed-off-by: Login Victor <batazor111@gmail.com>

--- ops/Makefile/k8s.mk
+++ ops/Makefile/k8s.mk
@@ -5,11 +5,12 @@ include $(SELF_DIR)/ops/Makefile/k8s.velero.mk
 # KUBERNETES TASKS =====================================================================================================
 PATH_TO_COMMON_CHART := ops/Helm/common
 
-SHORTLINK_NAMESPACE    := shortlink
-SHORTLINK_HELM_API     := ops/Helm/shortlink-api
-SHORTLINK_HELM_LOGGER  := ops/Helm/shortlink-logger
-SHORTLINK_HELM_UI      := ops/Helm/shortlink-ui
-SHORTLINK_HELM_INGRESS := ops/Helm/ingress
+SHORTLINK_NAMESPACE     := shortlink
+SHORTLINK_HELM_API      := ops/Helm/shortlink-api
+SHORTLINK_HELM_LOGGER   := ops/Helm/shortlink-logger
+SHORTLINK_HELM_METADATA := ops/Helm/shortlink-metadata
+SHORTLINK_HELM_UI       := ops/Helm/shortlink-ui
+SHORTLINK_HELM_INGRESS  := ops/Helm/ingress
 
 helm-init: ## helm init
 	# add custom repo for helm
@@ -18,12 +19,19 @@ helm-init: ## helm init
 	@helm repo add stable https://kubernetes-charts.storage.googleapis.com
 	@helm repo update
 
-helm-lint: ## Check Helm chart
+helm-lint: ## Check Helm chart by linter
 	@helm lint ${PATH_TO_COMMON_CHART}
 	@helm lint ${SHORTLINK_HELM_API}
 	@helm lint ${SHORTLINK_HELM_LOGGER}
+	@helm lint ${SHORTLINK_HELM_METADATA}
 	@helm lint ${SHORTLINK_HELM_UI}
 	@helm lint ${SHORTLINK_HELM_INGRESS}
+
+helm-test: ### Check Helm chart by run
+	@docker run -it \
+		-v /var/run/docker.sock:/var/run/docker.sock \
+		-v $(pwd):/home \
+		quay.io/helmpack/chart-testing bash -c "cd /home && ct lint --all --config ct.yaml"
 
 helm-common: ## run common service for
 	@make helm-init
