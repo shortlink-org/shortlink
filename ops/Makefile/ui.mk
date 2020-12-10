d447531db0de51f599c48b585cdcbac8ef48ff6e
Login Victor <batazor111@gmail.com>
2020-12-10 06:47:45 +0300

    next: fix
    Signed-off-by: Login Victor <batazor111@gmail.com>

--- ops/Makefile/ui.mk
+++ ops/Makefile/ui.mk
@@ -1,6 +1,12 @@
-# UI TASKS =============================================================================================================
+# NUXT TASKS ===========================================================================================================
 PATH_TO_UI_NUXT := pkg/ui/nuxt
 
 nuxt_generate: ## Deploy nuxt UI
 	@npm --prefix ${PATH_TO_UI_NUXT} install
 	@npm --prefix ${PATH_TO_UI_NUXT} run generate
+
+# NEXT TASKS ===========================================================================================================
+PATH_TO_UI_NEXT := pkg/ui/next
+
+next_run: ## Dev-mode Next UI
+	@npm --prefix ${PATH_TO_UI_NUXT} start
