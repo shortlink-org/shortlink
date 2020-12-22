4a666172bd112ae3cd234bb328415f896f6bc907
Victor Login <batazor111@gmail.com>
2020-12-22 06:25:09 +0300

    Update install-protobuf.sh

--- ops/scripts/install-protobuf.sh
+++ ops/scripts/install-protobuf.sh
@@ -2,7 +2,7 @@
 set -ex
 
 # TODO: refactoring this script
-PROTOBUF_VERSION=3.13.0
+PROTOBUF_VERSION=3.14.0
 basename=protoc-$PROTOBUF_VERSION-linux-x86_64.zip
 
 wget https://github.com/protocolbuffers/protobuf/releases/download/v$PROTOBUF_VERSION/$basename
