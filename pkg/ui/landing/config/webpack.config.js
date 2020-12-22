32e8fb691a71fc51f25448ac80c8f80ef3fc0e6c
Login Victor <batazor111@gmail.com>
2020-12-22 12:32:03 +0300

    landing: fix dep
    Signed-off-by: Login Victor <batazor111@gmail.com>

--- pkg/ui/landing/config/webpack.config.js
+++ pkg/ui/landing/config/webpack.config.js
@@ -13,6 +13,7 @@ module.exports = {
   output: {
     path: path.join(config.root, config.paths.dist),
     filename: '[name].[hash].js',
+    publicPath: "/landing/",
   },
   mode: ['production', 'development'].includes(config.env)
     ? config.env
