bf0f51e0e32eaa37b5f725671dafde65eeb6a047
Login Victor <batazor111@gmail.com>
2020-12-23 11:34:17 +0300

    fix test
    Signed-off-by: Login Victor <batazor111@gmail.com>

--- ops/scripts/coverage.sh
+++ ops/scripts/coverage.sh
@@ -3,7 +3,7 @@ set -ex
 
 echo "" > coverage.txt
 
-for d in $(go list ./... | grep -v vendor); do
+for d in $(go list ./... | grep -v vendor | grep -v node_modules); do
     go test -race -coverprofile=profile.out -covermode=atomic "${d}"
     if [ -f profile.out ]; then
         cat profile.out >> coverage.txt
