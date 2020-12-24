2ce170c5858afd2759d6597365c4a3c9d75e5636
Login Victor <batazor111@gmail.com>
2020-12-24 21:30:16 +0300

    fix after big refactoring project
    Signed-off-by: Login Victor <batazor111@gmail.com>

--- pkg/api/grpc-web/api.pb.gw.go
+++ pkg/api/grpc-web/api.pb.gw.go
@@ -13,7 +13,7 @@ import (
 	"io"
 	"net/http"
 
-	"github.com/batazor/shortlink/internal/api/domain/link"
+	"github.com/batazor/shortlink/internal/services/api/domain/link"
 	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	"github.com/grpc-ecosystem/grpc-gateway/v2/utilities"
 	"google.golang.org/grpc"
