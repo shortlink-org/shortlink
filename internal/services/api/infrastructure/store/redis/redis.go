e804ca6af949d826fff432539936c6cc8fcd022b
Login Victor <batazor111@gmail.com>
2021-02-07 00:07:09 +0300

    upd dep
    Signed-off-by: Login Victor <batazor111@gmail.com>

--- internal/services/api/infrastructure/store/redis/redis.go
+++ internal/services/api/infrastructure/store/redis/redis.go
@@ -5,7 +5,7 @@ import (
 	"encoding/json"
 	"fmt"
 
-	"github.com/go-redis/redis"
+	"github.com/go-redis/redis/v8"
 
 	"github.com/batazor/shortlink/internal/pkg/db"
 	"github.com/batazor/shortlink/internal/services/api/domain/link"
