f5e7c79a261ee2b10aaeeb18e5679ebb44fcfa4d
Login Victor <batazor111@gmail.com>
2021-03-17 03:24:33 +0300

    skaffold: improve
    Signed-off-by: Login Victor <batazor111@gmail.com>

--- ops/Makefile/k8s/skaffold.mk
+++ ops/Makefile/k8s/skaffold.mk
@@ -4,9 +4,7 @@ skaffold-init: ## Run local minikube and set default params
 	-kubectl create namespace shortlink
 
 skaffold-up: ## Run local minikube and set default params
-	@skaffold dev
+	@skaffold dev --port-forward
 
 skaffold-debug: ## Run local minikube and set default params with debug mode
 	@skaffold debug
-
-skaffold-down: ## Down dev-workspace
