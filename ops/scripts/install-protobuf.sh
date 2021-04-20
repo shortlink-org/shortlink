c0605ffc7e41f9fb96f562289c62cd1279c66b57
Login Victor <batazor111@gmail.com>
2021-04-20 03:21:48 +0300

    protoc: update to 3.15.8
    Signed-off-by: Login Victor <batazor111@gmail.com>

--- ops/scripts/install-protobuf.sh
+++ ops/scripts/install-protobuf.sh
@@ -2,12 +2,11 @@
 set -ex
 
 # TODO: refactoring this script
-PROTOBUF_VERSION=3.15.7
+PROTOBUF_VERSION=3.15.8
 basename=protoc-$PROTOBUF_VERSION-linux-x86_64.zip
 
 wget https://github.com/protocolbuffers/protobuf/releases/download/v$PROTOBUF_VERSION/$basename
 unzip -o $basename
 rm $basename
-rm -rf include
-rm -rf readme.txt
 mv bin/protoc /home/$USER/.local/bin/protoc
+rm -rf include readme.txt bin
