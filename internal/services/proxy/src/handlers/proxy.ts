811bf7fa885c32877b754d8d6bd60439ad9c3de9
Login Victor <batazor111@gmail.com>
2021-05-06 04:05:54 +0300

    proxy: step-by-step
    Signed-off-by: Login Victor <batazor111@gmail.com>

--- internal/services/proxy/src/handlers/proxy.ts
+++ internal/services/proxy/src/handlers/proxy.ts
@@ -1,4 +1,4 @@
-import * as grpc from 'grpc'
+import * as grpc from '@grpc/grpc-js'
 import { Timestamp } from 'google-protobuf/google/protobuf/timestamp_pb';
 
 import {Stats} from "../proto/domain/proxy/v1/proxy_pb";
@@ -6,13 +6,12 @@ import { StatsRequest, StatsResponse } from '../proto/infrastructure/rpc/v1/prox
 import { ProxyService, IProxyServer } from '../proto/infrastructure/rpc/v1/proxy_grpc_pb'
 
 class ProxyHandler implements IProxyServer {
-  [name: string]: import("@grpc/grpc-js").UntypedHandleCall;
-
+  [name: string]: grpc.UntypedHandleCall;
   /**
    * Return stats by use URL
    * @param url
    */
-  stats = (url: grpc.ServerUnaryCall<StatsRequest>): StatsResponse => {
+  stats = (url: grpc.ServerUnaryCall<StatsRequest, StatsResponse>): StatsResponse => {
     console.log(123)
     const resp: StatsResponse = new StatsResponse()
 
