039d75c3b75fecd7ead1d9d3d90f5808903a5361
Login Victor <batazor111@gmail.com>
2021-05-16 04:00:15 +0300

    telegram-bot (JAVA): create link
    Signed-off-by: Login Victor <batazor111@gmail.com>

--- internal/services/bot/src/main/java/Main.java
+++ internal/services/bot/src/main/java/Main.java
@@ -1,19 +1,40 @@
-import org.apache.log4j.LogManager;
-import org.apache.log4j.Logger;
+import com.rabbitmq.client.DeliverCallback;
+import http.LinkApp;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
-public class Main {
-  final static Logger logger = LogManager.getLogger(Main.class);
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.util.concurrent.TimeoutException;
 
-  public static void main(String[] args) throws TelegramApiException {
+public class Main {
+
+  public static void main(String[] args) throws TelegramApiException, IOException, TimeoutException, URISyntaxException, NoSuchAlgorithmException, KeyManagementException {
+    Logger logger = LoggerFactory.getLogger(Main.class);
+
+    // Init API
+    LinkApp api = new LinkApp();
+
+    // Subscribe on AMQP, exchange: 'shortlink'
+    DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+      String message = new String(delivery.getBody(), "UTF-8");
+      System.out.println(" [x] Received '" + message + "'");
+    };
+
+    // RabbitMQ
+    RabbitMQ rabbitmq = new RabbitMQ(deliverCallback);
+
     // Instantiate Telegram Bots API
     TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
 
     // Register our bot
     try {
-      telegramBotsApi.registerBot(new TelegramBot());
+      telegramBotsApi.registerBot(new TelegramBot(rabbitmq, api));
     } catch (TelegramApiException e) {
       e.printStackTrace();
     }
---
+++ internal/services/bot/src/main/java/RabbitMQ.java
@@ -0,0 +1,36 @@
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.DeliverCallback;
+import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.util.concurrent.TimeoutException;
+
+public class RabbitMQ {
+  private final static String EXCHANGE_NAME = "shortlink";
+  private final static String QUEUE_NAME = "shortlink_bot";
+  private String MQ_RABBIT_URI = System.getenv("MQ_RABBIT_URI");
+  private Channel channel;
+
+  public RabbitMQ(DeliverCallback deliverCallback) throws IOException, TimeoutException, URISyntaxException, NoSuchAlgorithmException, KeyManagementException {
+    ConnectionFactory factory = new ConnectionFactory();
+
+    factory.setUri(MQ_RABBIT_URI);
+    Connection connection = factory.newConnection();
+    this.channel = connection.createChannel();
+
+    this.channel.exchangeDeclare(EXCHANGE_NAME, "fanout");
+    this.channel.queueDeclare(QUEUE_NAME, true, false, false, null);
+    this.channel.queueBind(QUEUE_NAME, EXCHANGE_NAME, "");
+
+    this.channel.basicConsume(QUEUE_NAME, true, deliverCallback, consumerTag -> { });
+  }
+
+  public void Send(@NotNull String message) throws IOException {
+    this.channel.basicPublish(EXCHANGE_NAME, "", null, message.getBytes("UTF-8"));
+  }
+}
