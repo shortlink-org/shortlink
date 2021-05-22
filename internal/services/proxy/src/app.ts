0f045dbcf126c89d72ec885fc4dd30a13b155572
Login Victor <batazor111@gmail.com>
2021-05-22 03:42:59 +0300

    proxy: add readiness / liveness checks
    Signed-off-by: Login Victor <batazor111@gmail.com>

--- internal/services/proxy/src/app.ts
+++ internal/services/proxy/src/app.ts
@@ -1,9 +1,11 @@
 import "reflect-metadata"
+import http from "http"
 import * as express from "express"
 import * as bodyParser from 'body-parser'
 import helmet from 'helmet'
 import { Logger } from "tslog"
 import { InversifyExpressServer } from "inversify-express-utils"
+import { createTerminus } from "@godaddy/terminus"
 
 import container from "./inversify.config"
 
@@ -12,6 +14,7 @@ import './proxy/infrastructure/http/proxy'
 const APP = express.default()
 const PORT = process.env.PORT || 3000
 const log: Logger = new Logger()
+const SERVER_HTTP = http.createServer(APP)
 
 // configuration application
 APP.use(bodyParser.urlencoded({
@@ -23,6 +26,18 @@ APP.use(helmet())
 // start the server
 const SERVER = new InversifyExpressServer(container, null, { rootPath: "/" }, APP);
 const APP_CONFIGURED = SERVER.build();
-APP_CONFIGURED.listen(PORT, () => log.info(`App running on ${PORT}`));
 
-exports = module.exports = APP_CONFIGURED
+async function onHealthCheck () {
+  // checks if the system is healthy, like the db connection is live
+  // resolves, if health, rejects if not
+}
+
+// graceful shutdown and Kubernetes readiness/liveness checks
+createTerminus(SERVER_HTTP, {
+  signal: 'SIGINT',
+  healthChecks: { '/ready': onHealthCheck },
+})
+
+SERVER_HTTP.listen(PORT, () => log.info(`App running on ${PORT}`));
+
+exports = module.exports = SERVER_HTTP
