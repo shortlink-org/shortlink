ddf3ad7e65ce8c7e496d7d1f2847e163ea957886
Login Victor <batazor111@gmail.com>
2021-06-05 21:34:51 +0300

    ui-next: fix generate
    Signed-off-by: Login Victor <batazor111@gmail.com>

--- ui/next/pages/auth/login.js
+++ ui/next/pages/auth/login.js
@@ -1,19 +1,14 @@
 import React from 'react';
-import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
 import Link from '@material-ui/core/Link';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper'
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
-import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
 import { Layout } from '../../components';
-import { Google, Facebook, GitHub } from './oAuthServices.js'
+import { Google, Facebook, GitHub } from '../../components/widgets/oAuthServices.js'
 
 const useStyles = makeStyles((theme) => ({
   paper: {
