b2977180dccb73423a03c98f1c49d49a0a0e0e17
Login Victor <batazor111@gmail.com>
2021-06-07 12:09:35 +0300

    auth: improve page
    Signed-off-by: Login Victor <batazor111@gmail.com>

--- ui/next/pages/pricing.tsx
+++ ui/next/pages/pricing.tsx
@@ -124,6 +124,35 @@ export function PricingContent() {
           with this layout. It&apos;s built with default Material-UI components
           with little customization.
         </Typography>
+
+        <div className="flex flex-col justify-center text-xs text-gray-600 md:flex-row">
+          <div className="flex items-center p-4">
+            <svg viewBox="0 0 20 20" fill="currentColor" className="w-4 h-4 mr-1 text-green-600">
+              <path
+                fillRule="evenodd"
+                d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                clipRule="evenodd"
+              />
+            </svg>
+            No credit card required
+          </div>
+          <div className="flex items-center p-4">
+            <svg viewBox="0 0 20 20" fill="currentColor" className="w-4 h-4 mr-1 text-green-600">
+              <path fillRule="evenodd"
+                    d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                    clipRule="evenodd"/>
+            </svg>
+            14 days free
+          </div>
+          <div className="flex items-center p-4">
+            <svg viewBox="0 0 20 20" fill="currentColor" className="w-4 h-4 mr-1 text-green-600">
+              <path fillRule="evenodd"
+                    d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                    clipRule="evenodd"/>
+            </svg>
+            Cancel anytime
+          </div>
+        </div>
       </Container>
 
       <Discounted />
