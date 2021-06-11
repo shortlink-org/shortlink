b794580035b96d08b334c53a46cfc1c5ca8a8ee5
Login Victor <batazor111@gmail.com>
2021-06-11 04:18:21 +0300

    next-ui: add sentry
    Signed-off-by: Login Victor <batazor111@gmail.com>

---
+++ ui/next/README.md
@@ -0,0 +1,15 @@
+## NEXT-UI Application
+
+This UI for shortlink application
+
+#### Feature/Require
+
+- NextJS (SSR/Static generate content)
+- Auth (Kratos)
+- Monitoring (Sentry)
+- Pretty UI
+  - tailwind CSS
+  - Material-UI
+- Pretty code base
+  - Typescript
+  - ESLint/Prettier
--- ui/next/next.config.js
+++ ui/next/next.config.js
@@ -1,7 +1,11 @@
 const webpack = require('webpack')
 const withSourceMaps = require('@zeit/next-source-maps')
+
+// ENVIRONMENT VARIABLE ================================================================================================
 const isProd = process.env.NODE_ENV === 'production'
 const API_URI_PORT = process.env.API_URI_PORT || 7070
+const isEnableSentry = process.env.SENTRY_ENABLE
+
 // TODO: fix DNS
 const API_URI = process.env.API_URI ? `${process.env.API_URI}:${API_URI_PORT}` : `http://localhost:${API_URI_PORT}`
 const PROXY_URI = process.env.PROXY_URI || 'http://proxy-shortlink-proxy.shortlink:3030'
@@ -55,4 +59,29 @@ if (!isProd) {
   }
 }
 
-module.exports = withSourceMaps(NEXT_CONFIG)
+let EXPORT_CONFIG = withSourceMaps(NEXT_CONFIG)
+
+// Make sure adding Sentry options is the last code to run before exporting, to
+// ensure that your source maps include changes from all other Webpack plugins
+if (isEnableSentry) {
+  // This file sets a custom webpack configuration to use your Next.js app
+  // with Sentry.
+  // https://nextjs.org/docs/api-reference/next.config.js/introduction
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/
+  const { withSentryConfig } = require('@sentry/nextjs');
+
+  const SentryWebpackPluginOptions = {
+    // Additional config options for the Sentry Webpack plugin. Keep in mind that
+    // the following options are set automatically, and overriding them is not
+    // recommended:
+    //   release, url, org, project, authToken, configFile, stripPrefix,
+    //   urlPrefix, include, ignore
+    // For all available options, see:
+    // https://github.com/getsentry/sentry-webpack-plugin#options.
+  };
+
+  EXPORT_CONFIG = withSentryConfig(EXPORT_CONFIG, SentryWebpackPluginOptions);
+}
+
+module.exports = EXPORT_CONFIG
+
