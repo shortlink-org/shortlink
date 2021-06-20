bebbbfc5d53211e57d0f545cf28c3ec7538a91bf
Login Victor <batazor111@gmail.com>
2021-06-21 00:09:51 +0300

    proxy: improve work with buf.build
    Signed-off-by: Login Victor <batazor111@gmail.com>

--- ops/Makefile/proto.mk
+++ ops/Makefile/proto.mk
@@ -1,9 +1,16 @@
 # PROTO TASKS ==========================================================================================================
 
-proto-lint: ## Check lint (use buf check)
+proto-lint: ## Check lint
 	@buf ls-files
 	@buf lint
 	@buf breaking --against ops/proto-lock.json
 
 proto-lock: ## Lock proto dependencies
 	@buf build -o ops/proto-lock.json
+
+proto-generate: ## Generate proto-files
+	@buf generate \
+		--path=internal/services/proxy/src/proto/domain \
+		--path=internal/services/proxy/src/proto/infrastructure \
+		--template=ops/proto/proxy/buf.gen.yaml \
+		--config=ops/proto/proxy/buf.yaml
