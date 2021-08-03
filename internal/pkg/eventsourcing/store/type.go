976119b75e9f7e1c28881d39221c5c98985a5ed5
Login Victor <batazor111@gmail.com>
2021-08-03 16:08:47 +0300

    event-sourcing: refactoring
    Signed-off-by: Login Victor <batazor111@gmail.com>

--- internal/pkg/eventsourcing/store/type.go
+++ internal/pkg/eventsourcing/store/type.go
@@ -2,9 +2,6 @@ package event_store
 
 import (
 	"context"
-	"time"
-
-	"github.com/google/uuid"
 
 	"github.com/batazor/shortlink/internal/pkg/db"
 	eventsourcing "github.com/batazor/shortlink/internal/pkg/eventsourcing/v1"
@@ -30,28 +27,4 @@ type Repository struct { // nolint unused
 
 	// system event
 	notify.Subscriber // Observer interface for subscribe on system event
-}
-
-// Stream represents ordered set of events. Simply speaking stream is an log of the events
-// that happened for the specific aggregate/entity.
-type Stream struct {
-	Id      uuid.UUID // needs to be unique
-	Types   uuid.UUID // information about the stream type. It' mostly used to make debugging easier or some optimizations
-	Version int       // auto-incremented value used mostly for optimistic concurrency check.
-}
-
-/*
-	Event table is the main table for Event Sourcing storage.
-	It contains the information about events that occurred in the system.
-	Each event is stored in separate row.
-
-	They're stored as key/value pair (id + event data) plus additional data like stream id, version, type, creation timestamp.
-*/
-type Event struct {
-	Id        uuid.UUID // unique event identifier
-	Payload   string    // event data serialized as JSON
-	StreamId  uuid.UUID // id of the stream that event occurred
-	Type      uuid.UUID // information about the event type. It' mostly used to make debugging easier or some optimizations.
-	Version   int32     // version of the stream at which event occurred used for keeping sequence of the event and for optimistic concurrency check
-	CreatedAt time.Time // Timestamp at which event was created. Used to get the state of the stream at exact time.
 }
