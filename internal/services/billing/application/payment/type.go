16968ce36b99e8f399f889e00faccc903d65da1d
Login Victor <batazor111@gmail.com>
2021-08-04 14:11:41 +0300

    event-sourcing: add eventBus
    Signed-off-by: Login Victor <batazor111@gmail.com>

--- internal/services/billing/application/payment/type.go
+++ internal/services/billing/application/payment/type.go
@@ -2,10 +2,24 @@ package payment_application
 
 import (
 	eventsourcing "github.com/batazor/shortlink/internal/pkg/eventsourcing/v1"
+	"github.com/batazor/shortlink/internal/pkg/notify"
 	billing "github.com/batazor/shortlink/internal/services/billing/domain/billing/payment/v1"
 )
 
 type Payment struct {
 	*eventsourcing.BaseAggregate
 	*billing.Payment
+}
+
+var (
+	// EventList - event notify list
+	EventList map[string]uint32
+)
+
+func init() {
+	EventList = make(map[string]uint32, 0)
+
+	for event := range billing.Event_name {
+		EventList[billing.Event_name[event]] = notify.NewEventID()
+	}
 }
