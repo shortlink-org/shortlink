9ccf9b7661d8df122b057a73bc26ba5ae9f3d738
Login Victor <batazor111@gmail.com>
2021-08-07 04:52:02 +0300

    cqrs: add migration for link service
    Signed-off-by: Login Victor <batazor111@gmail.com>

---
+++ internal/pkg/db/postgres/migrations/000006_cqrs_link_view.up.sql
@@ -0,0 +1,21 @@
+-- BILLING SCHEMA ======================================================================================================
+
+CREATE SCHEMA shortlink;
+
+COMMENT ON SCHEMA shortlink IS 'Shortlink schema';
+
+-- CQRS for links ======================================================================================================
+create table shortlink.link_view
+(
+	id UUID default uuid_generate_v4() not null,
+	url varchar(255) not null,
+	hash varchar(20) not null,
+	describe text,
+	created_at TIMESTAMP default current_timestamp,
+	updated_at TIMESTAMP default current_timestamp
+);
+
+comment on table shortlink.link_view is 'CQRS for links';
+
+create index link_view_hash_index
+	on shortlink.link_view (hash);
