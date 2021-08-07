3a737df1b9f43cf4503ae0657612c46c280c40f3
Login Victor <batazor111@gmail.com>
2021-08-08 01:49:10 +0300

    cqrs: link service use lint_view table
    Signed-off-by: Login Victor <batazor111@gmail.com>

--- internal/services/link/application/link/event.go
+++ internal/services/link/application/link/event.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/batazor/shortlink/internal/pkg/notify"
 	"github.com/batazor/shortlink/internal/services/api/domain"
+	v1 "github.com/batazor/shortlink/internal/services/link/domain/link/v1"
 )
 
 func (s *Service) EventHandler() {
@@ -23,14 +24,26 @@ func (s *Service) Notify(ctx context.Context, event uint32, payload interface{})
 	switch event {
 	case api_domain.METHOD_ADD:
 		{
+			_, err := s.cqsStore.Add(ctx, payload.(*v1.Link))
+			if err != nil {
+				s.logger.ErrorWithContext(ctx, err.Error())
+			}
 			return notify.Response{}
 		}
 	case api_domain.METHOD_UPDATE:
 		{
+			_, err := s.cqsStore.Update(ctx, payload.(*v1.Link))
+			if err != nil {
+				s.logger.ErrorWithContext(ctx, err.Error())
+			}
 			return notify.Response{}
 		}
 	case api_domain.METHOD_DELETE:
 		{
+			err := s.cqsStore.Delete(ctx, payload.(string))
+			if err != nil {
+				s.logger.ErrorWithContext(ctx, err.Error())
+			}
 			return notify.Response{}
 		}
 	default:
