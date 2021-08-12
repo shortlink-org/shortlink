864451223de65bba77abda82d1c3baa3b3a74b88
Login Victor <batazor111@gmail.com>
2021-08-13 02:51:55 +0300

    bdd: add template for golang
    Signed-off-by: Login Victor <batazor111@gmail.com>

---
+++ internal/services/logger/tests/main_test.go
@@ -0,0 +1,42 @@
+package tests
+
+import (
+	"os"
+	"testing"
+
+	"github.com/cucumber/godog"
+	"github.com/cucumber/godog/colors"
+	"github.com/spf13/pflag"
+)
+
+var opts = godog.Options{
+	Format: "progress",
+	Output: colors.Colored(os.Stdout),
+}
+
+func init() {
+	godog.BindCommandLineFlags("godog.", &opts) // godog v0.11.0 and later
+}
+
+func TestMain(m *testing.M) {
+	pflag.Parse()
+	opts.Paths = pflag.Args()
+
+	status := godog.TestSuite{
+		Name:                 "godogs",
+		TestSuiteInitializer: InitializeTestSuite,
+		ScenarioInitializer:  InitializeScenario,
+		Options:              &opts,
+	}.Run()
+
+	// Optional: Run `testing` package's logic besides godog.
+	if st := m.Run(); st > status {
+		status = st
+	}
+
+	os.Exit(status)
+}
+
+func InitializeTestSuite(ctx *godog.TestSuiteContext) {}
+
+func InitializeScenario(ctx *godog.ScenarioContext) {}
