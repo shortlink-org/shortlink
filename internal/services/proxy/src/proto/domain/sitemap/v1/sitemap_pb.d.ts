d1c9c08ddc85e29c8b58865818cd68249b792f6f
Login Victor <batazor111@gmail.com>
2021-08-16 04:17:19 +0300

    sitemap: use buf
    Signed-off-by: Login Victor <batazor111@gmail.com>

--- internal/services/proxy/src/proto/domain/sitemap/v1/sitemap_pb.d.ts
+++ internal/services/proxy/src/proto/domain/sitemap/v1/sitemap_pb.d.ts
@@ -6,12 +6,42 @@
 
 import * as jspb from "google-protobuf";
 import * as tagger_tagger_pb from "../../../tagger/tagger_pb";
+import * as google_protobuf_timestamp_pb from "google-protobuf/google/protobuf/timestamp_pb";
+
+export class Url extends jspb.Message { 
+    getLoc(): string;
+    setLoc(value: string): Url;
+    getLastMod(): string;
+    setLastMod(value: string): Url;
+    getChangeFreq(): string;
+    setChangeFreq(value: string): Url;
+    getPriority(): number;
+    setPriority(value: number): Url;
+
+    serializeBinary(): Uint8Array;
+    toObject(includeInstance?: boolean): Url.AsObject;
+    static toObject(includeInstance: boolean, msg: Url): Url.AsObject;
+    static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+    static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
+    static serializeBinaryToWriter(message: Url, writer: jspb.BinaryWriter): void;
+    static deserializeBinary(bytes: Uint8Array): Url;
+    static deserializeBinaryFromReader(message: Url, reader: jspb.BinaryReader): Url;
+}
+
+export namespace Url {
+    export type AsObject = {
+        loc: string,
+        lastMod: string,
+        changeFreq: string,
+        priority: number,
+    }
+}
 
 export class Sitemap extends jspb.Message { 
-    getLocation(): string;
-    setLocation(value: string): Sitemap;
-    getLastModified(): string;
-    setLastModified(value: string): Sitemap;
+    clearUrlList(): void;
+    getUrlList(): Array<Url>;
+    setUrlList(value: Array<Url>): Sitemap;
+    addUrl(value?: Url, index?: number): Url;
 
     serializeBinary(): Uint8Array;
     toObject(includeInstance?: boolean): Sitemap.AsObject;
@@ -25,7 +55,6 @@ export class Sitemap extends jspb.Message {
 
 export namespace Sitemap {
     export type AsObject = {
-        location: string,
-        lastModified: string,
+        urlList: Array<Url.AsObject>,
     }
 }
