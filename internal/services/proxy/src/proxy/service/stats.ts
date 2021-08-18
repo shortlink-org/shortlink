4eb067478717c2940a5a03fefb9354858ba827d7
Login Victor <batazor111@gmail.com>
2021-08-18 20:37:28 +0300

    proxy: update schema
    Signed-off-by: Login Victor <batazor111@gmail.com>

--- internal/services/proxy/src/proxy/service/stats.ts
+++ internal/services/proxy/src/proxy/service/stats.ts
@@ -19,7 +19,7 @@ export class StatsService {
     @inject(TYPES.REPOSITORY.StatsRepository) private readonly store: StatsRepository,
     @inject(TYPES.TAGS.AMQPController) private readonly amqp: AMQPController,
   ) {
-    const exchange = this.amqp.connection.declareExchange(MQ_EVENT_LINK_NEW, "fanout", {durable: false})
+    const exchange = this.amqp.connection.declareExchange(MQ_EVENT_LINK_NEW, "fanout", {durable: true})
     let queue = this.amqp.connection.declareQueue("shortlink-proxy")
     queue.bind(exchange)
     queue.activateConsumer((message: Amqp.Message) => {
