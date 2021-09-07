35a063fb52cb0af9416af014e278734ee6c2c5d0
Login Victor <batazor111@gmail.com>
2021-09-07 05:24:23 +0300

    minikube: add tracing config
    Signed-off-by: Login Victor <batazor111@gmail.com>

--- ops/Makefile/k8s/minikube.mk
+++ ops/Makefile/k8s/minikube.mk
@@ -4,6 +4,7 @@ minikube-up: ## run minikube for dev mode
 	# for enable audit
 	@mkdir -p ~/.minikube/files/etc/ssl/certs
 	@cp ${PWD}/ops/Makefile/conf/audit-policy.yaml ~/.minikube/files/etc/ssl/certs/audit-policy.yaml
+	@cp ${PWD}/ops/Makefile/conf/tracing-config-file.yaml ~/.minikube/files/etc/ssl/certs/tracing-config-file.yaml
 
 	@minikube start \
 		--nodes 3 \
@@ -13,6 +14,8 @@ minikube-up: ## run minikube for dev mode
 		--container-runtime=containerd \
 		--listen-address=0.0.0.0 \
 		--addons=pod-security-policy,ingress \
+		--feature-gates="GracefulNodeShutdown=true" \
+		--extra-config=apiserver.tracing-config-file=/etc/ssl/certs/tracing-config-file.yaml \
 		--extra-config=apiserver.service-account-signing-key-file=/var/lib/minikube/certs/sa.key \
 		--extra-config=apiserver.service-account-key-file=/var/lib/minikube/certs/sa.pub \
 		--extra-config=apiserver.service-account-issuer=api \
