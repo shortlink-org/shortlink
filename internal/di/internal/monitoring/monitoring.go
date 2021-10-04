a1546b0a8e3991a4e3c4617c2baead5f034d4536
Login Victor <batazor111@gmail.com>
2021-10-04 09:21:14 +0300

    devops: add monitoring for api service
    Signed-off-by: Login Victor <batazor111@gmail.com>

--- internal/di/internal/monitoring/monitoring.go
+++ internal/di/internal/monitoring/monitoring.go
@@ -6,6 +6,7 @@ import (
 	sentryhttp "github.com/getsentry/sentry-go/http"
 	"github.com/heptiolabs/healthcheck"
 	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/collectors"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 
 	"github.com/batazor/shortlink/internal/pkg/logger"
@@ -18,7 +19,7 @@ func New(sentryHandler *sentryhttp.Handler, log logger.Logger) *http.ServeMux {
 	registry := prometheus.NewRegistry()
 
 	// Add Go module build info.
-	registry.MustRegister(prometheus.NewBuildInfoCollector()) // nolint staticcheck
+	registry.MustRegister(collectors.NewBuildInfoCollector())
 
 	// Create a metrics-exposing Handler for the Prometheus registry
 	// The healthcheck related metrics will be prefixed with the provided namespace
