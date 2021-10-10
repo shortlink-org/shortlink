81dff481057abaf3b351d2332acdf9a10cb0ae43
Login Victor <batazor111@gmail.com>
2021-10-10 20:44:41 +0300

    newsletter: add routing
    Signed-off-by: Login Victor <batazor111@gmail.com>

--- internal/services/newsletter/src/main.rs
+++ internal/services/newsletter/src/main.rs
@@ -4,13 +4,10 @@ use std::convert::Infallible;
 
 use hyper::service::{make_service_fn, service_fn};
 use hyper::{Body, Request, Response, Server};
-
-async fn hello(_: Request<Body>) -> Result<Response<Body>, Infallible> {
-    Ok(Response::new(Body::from("Hello World!")))
-}
+use hyper::{Method, StatusCode};
 
 #[tokio::main]
-pub async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+pub async fn main() {
     pretty_env_logger::init();
 
     // For every connection, we must make a `Service` to handle all
@@ -22,13 +19,37 @@ pub async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
         async { Ok::<_, Infallible>(service_fn(hello)) }
     });
 
+    // We'll bind to 127.0.0.1:3000
     let addr = ([127, 0, 0, 1], 3000).into();
 
     let server = Server::bind(&addr).serve(make_svc);
 
     println!("Listening on http://{}", addr);
 
-    server.await?;
+    // And now add a graceful shutdown signal...
+    let graceful = server.with_graceful_shutdown(shutdown_signal());
 
-    Ok(())
+    // Run this server for... forever!
+    if let Err(e) = graceful.await {
+        eprintln!("server error: {}", e);
+    }
+}
+
+async fn hello(req: Request<Body>) -> Result<Response<Body>, Infallible> {
+    let mut response = Response::new(Body::empty());
+    match (req.method(), req.uri().path()) {
+        (&Method::GET, "/newsletters") => { *response.body_mut() = Body::from("get list subscribes"); },
+        (&Method::POST, "/newsletter") => { *response.body_mut() = Body::from("add newsletter subscribes"); },
+        (&Method::GET, "/newsletter/unsubscribe") => { *response.body_mut() = Body::from("newsletter unsubscribes"); },
+        _ => { *response.status_mut() = StatusCode::NOT_FOUND; },
+    };
+
+    Ok(response)
+}
+
+async fn shutdown_signal() {
+    // Wait for the CTRL+C signal
+    tokio::signal::ctrl_c()
+        .await
+        .expect("failed to install CTRL+C signal handler");
 }
