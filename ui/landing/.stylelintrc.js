2f0943a1ca44e1d7e1f2d7987888310e91783c37
Login Victor <batazor111@gmail.com>
2021-10-22 04:06:08 +0300

    landing: fix deps
    Signed-off-by: Login Victor <batazor111@gmail.com>

--- ui/landing/.stylelintrc.js
+++ ui/landing/.stylelintrc.js
@@ -1,9 +1,14 @@
 module.exports = {
+  "customSyntax": "postcss-scss",
   "extends": "stylelint-config-standard",
   "plugins": ["stylelint-scss"],
   "rules": {
     "at-rule-no-unknown": null,
     "scss/at-rule-no-unknown": true,
-    "no-descending-specificity": null
+    "no-descending-specificity": null,
+    "max-line-length": 122,
+    "color-function-notation": "legacy",
+    "value-no-vendor-prefix": false,
+    "alpha-value-notation": "number"
   },
 }
