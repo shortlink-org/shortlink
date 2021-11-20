0484278fe7abea744cdd9a13805b2bcd50e2a0a3
Login Victor <batazor111@gmail.com>
2021-11-21 01:56:12 +0300

    aerospike: add go-package and docker-compose
    Signed-off-by: Login Victor <batazor111@gmail.com>

---
+++ internal/pkg/db/aerospike/aerospike.go
@@ -0,0 +1,78 @@
+package aerospike
+
+import (
+	"context"
+	"net/url"
+	"strconv"
+
+	aero "github.com/aerospike/aerospike-client-go"
+	"github.com/spf13/viper"
+)
+
+// Config ...
+type Config struct { // nolint unused
+	host string
+	port int
+}
+
+// Store implementation of db interface
+type Store struct { // nolint unused
+	client *aero.Client
+	config Config
+}
+
+// Init ...
+func (s *Store) Init(ctx context.Context) error {
+	// Set configuration
+	err := s.setConfig()
+	if err != nil {
+		return err
+	}
+
+	// Connect to Aerospike
+	s.client, err = aero.NewClient(s.config.host, s.config.port)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// GetConn ...
+func (s *Store) GetConn() interface{} {
+	return s.client
+}
+
+// Close ...
+func (s *Store) Close() error {
+	s.client.Close()
+	return nil
+}
+
+// Migrate ...
+func (s *Store) migrate() error { // nolint unused
+	return nil
+}
+
+// setConfig - set configuration
+func (s *Store) setConfig() error {
+	viper.AutomaticEnv()
+	viper.SetDefault("STORE_AEROSPIKE_URI", "tcp://localhost:3000") // Aerospike URI
+
+	conf, err := url.Parse(viper.GetString("STORE_AEROSPIKE_URI"))
+	if err != nil {
+		return nil
+	}
+
+	port, err := strconv.Atoi(conf.Port())
+	if err != nil {
+		return nil
+	}
+
+	s.config = Config{
+		host: conf.Hostname(),
+		port: port,
+	}
+
+	return nil
+}
