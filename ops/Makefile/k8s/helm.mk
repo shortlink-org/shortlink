e8519428554fcc23f75709ecdf3b589377865dac
Victor Login <batazor111@gmail.com>
2022-01-26 09:49:43 +0300

    Update helm.mk

--- ops/Makefile/k8s/helm.mk
+++ ops/Makefile/k8s/helm.mk
@@ -27,4 +27,4 @@ helm-lint: ## Check Helm chart by linter
 
 # HELM TASKS ===========================================================================================================
 helm-docs: ### Generate HELM docs
-	@docker run --rm --volume "$(pwd):/helm-docs" -u "$(id -u)" jnorwood/helm-docs:v1.5.0
+	@docker run --rm --volume "$(pwd):/helm-docs" -u "$(id -u)" jnorwood/helm-docs:v1.7.0
