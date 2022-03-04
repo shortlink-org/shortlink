2dfe61d224b287cebb9c9d5ef1817c82760d4267
Login Victor <batazor111@gmail.com>
2022-03-05 01:02:19 +0300

    step-by-step
    Signed-off-by: Login Victor <batazor111@gmail.com>

--- internal/services/proxy/src/proxy/service/links.ts
+++ internal/services/proxy/src/proxy/service/links.ts
@@ -10,8 +10,9 @@ export class LinkService {
   public async get(hash: string): Promise<string> {
     // TODO: use gRPC
     const resp = await fetch(`${process.env.API_LINK_SERVICE}/api/link/${hash}`)
-    const link: Link = await resp.json()
+    const link: unknown = await resp.json()
 
+    // @ts-ignore
     return link.url
   }
 }
