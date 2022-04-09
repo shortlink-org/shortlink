1933be666a80c7c3fbd358a47818a88df4de5e31
Login Victor <batazor111@gmail.com>
2022-04-09 03:43:45 +0300

    next-ui: auth work version
    Signed-off-by: Login Victor <batazor111@gmail.com>

--- ui/next/pkg/sdk/index.ts
+++ ui/next/pkg/sdk/index.ts
@@ -1,6 +1,9 @@
 import { Configuration, V0alpha2Api } from '@ory/client'
-const KRATOS_PUBLIC_API = process.env.KRATOS_PUBLIC_API || 'https://shortlink.ddns.net/api/auth'
+const KRATOS_PUBLIC_API = process.env.KRATOS_PUBLIC_API || 'http://127.0.0.1:4433'
 
 export default new V0alpha2Api(new Configuration({
   basePath: KRATOS_PUBLIC_API,
+  baseOptions: {
+    withCredentials: true,
+  }
 }))
