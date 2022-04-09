3ac39ae078b65d5fa79ffd38b67512eb41ce04ac
Login Victor <batazor111@gmail.com>
2022-04-10 02:20:07 +0300

    next-ui: step-by-step
    Signed-off-by: Login Victor <batazor111@gmail.com>

--- ui/next/pages/admin/groups.tsx
+++ ui/next/pages/admin/groups.tsx
@@ -2,6 +2,7 @@
 import React from 'react'
 import { Layout } from 'components'
 import withAuthSync from 'components/Private'
+import { BeakerIcon } from '@heroicons/react/solid'
 
 const people = [
   {
@@ -65,6 +66,16 @@ export function GroupContent() {
   return (
     <Layout>
       <section className="text-gray-600 body-font">
+        <a href="#"
+           className="group block max-w-xs mx-auto rounded-lg p-6 bg-white ring-1 ring-slate-900/5 shadow-lg space-y-3 hover:bg-sky-500 hover:ring-sky-500">
+          <div className="flex items-center space-x-3">
+            <BeakerIcon className="h-6 w-6 stroke-sky-500 group-hover:stroke-white" />
+            <h3 className="text-slate-900 group-hover:text-white text-sm font-semibold">New groups</h3>
+          </div>
+          <p className="text-slate-500 group-hover:text-white text-sm">Create a new project from a variety of starting
+            templates.</p>
+        </a>
+
         <div className="container px-5 py-10 mx-auto">
           <div className="flex flex-wrap -m-4 text-center">
             <div className="p-4 sm:w-1/4 w-1/2">
