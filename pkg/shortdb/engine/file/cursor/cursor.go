8108e6e39fb3f8359ab4a45ed76d92621d81bf31
Login Victor <batazor111@gmail.com>
2022-05-14 02:40:52 +0300

    shortdb: refactoring
    Signed-off-by: Login Victor <batazor111@gmail.com>

--- pkg/shortdb/engine/file/cursor/cursor.go
+++ pkg/shortdb/engine/file/cursor/cursor.go
@@ -21,8 +21,8 @@ func New(table *table.Table, isEnd bool) (*Cursor, error) {
 }
 
 func (c *Cursor) Advance() {
-	c.wc.Lock()
-	defer c.wc.Unlock()
+	c.wc.RLock()
+	defer c.wc.RUnlock()
 
 	if c.RowId > 0 && c.RowId%c.Table.Option.PageSize == 0 {
 		c.PageId = int32(c.RowId / c.Table.Option.PageSize)
@@ -36,13 +36,8 @@ func (c *Cursor) Advance() {
 }
 
 func (c *Cursor) Value() (*table.Row, error) {
-	c.wc.Lock()
-	defer c.wc.Unlock()
-	//if c.Table.Pages == nil || len(c.Table.Pages) < int(c.PageId)+1 || c.Table.Pages[c.PageId] == nil {
-	//	return nil, &ErrorGetPage{}
-	//}
-	//
-	//page := c.Table.Pages[c.PageId]
+	c.wc.RLock()
+	defer c.wc.RUnlock()
 
 	if c.Table.Pages == nil {
 		return nil, &ErrorGetPage{}
@@ -50,16 +45,13 @@ func (c *Cursor) Value() (*table.Row, error) {
 
 	page := c.Table.Pages[c.PageId]
 	if len(page.Rows) == 0 {
-		//c.wc.Lock()
 		page.Rows = make([]*table.Row, c.Table.Option.PageSize)
-		//c.wc.Unlock()
 	}
 
 	rowNum := int(c.RowId) % len(page.Rows)
 
 	if page.Rows[rowNum] == nil {
 		page.Rows[rowNum] = &table.Row{}
-		//return page.Rows[rowNum], &ErrorGetPage{}
 	}
 
 	return page.Rows[rowNum], nil
