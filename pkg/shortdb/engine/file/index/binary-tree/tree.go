6890c4c2ec7ebb304b95c22946d44ac45956c5ad
Login Victor <batazor111@gmail.com>
2022-06-28 19:21:41 +0300

    shortdb: index - save file
    Signed-off-by: Login Victor <batazor111@gmail.com>

--- pkg/shortdb/engine/file/index/binary-tree/tree.go
+++ pkg/shortdb/engine/file/index/binary-tree/tree.go
@@ -8,10 +8,10 @@ import (
 type Tree[T any] struct {
 	// cmp compares two T values.
 	cmp     func(T, T) int
-	left    *Tree[T]
-	right   *Tree[T]
-	val     *T
-	payload []byte
+	Left    *Tree[T]
+	Right   *Tree[T]
+	Val     *T
+	Payload []byte
 }
 
 func (t *Tree[T]) Marshal() ([]byte, error) {
@@ -30,19 +30,19 @@ func New[T any](cmp func(T, T) int) *Tree[T] {
 }
 
 func (t *Tree[T]) Find(key T) []byte {
-	return t.find(key).payload
+	return t.find(key).Payload
 }
 
 func (t *Tree[T]) find(key T) *Tree[T] {
-	if t.val == nil {
+	if t.Val == nil {
 		return t
 	}
 
-	switch cmp := t.cmp(key, *t.val); {
+	switch cmp := t.cmp(key, *t.Val); {
 	case cmp < 0:
-		return t.left.find(key)
+		return t.Left.find(key)
 	case cmp > 0:
-		return t.right.find(key)
+		return t.Right.find(key)
 	default:
 		return t
 	}
@@ -54,26 +54,26 @@ func (t *Tree[T]) Insert(key T) error {
 }
 
 func (t *Tree[T]) insert(key T) *Tree[T] {
-	if t.val == nil {
-		t.val = &key
+	if t.Val == nil {
+		t.Val = &key
 		return t
 	}
 
-	switch cmp := t.cmp(key, *t.val); {
+	switch cmp := t.cmp(key, *t.Val); {
 	case cmp < 0:
-		if t.left == nil {
-			t.left = New(t.cmp)
-			t.left.val = &key
-			return t.left
+		if t.Left == nil {
+			t.Left = New(t.cmp)
+			t.Left.Val = &key
+			return t.Left
 		}
-		return t.left.insert(key)
+		return t.Left.insert(key)
 	case cmp > 0:
-		if t.right == nil {
-			t.right = New(t.cmp)
-			t.right.val = &key
-			return t.right
+		if t.Right == nil {
+			t.Right = New(t.cmp)
+			t.Right.Val = &key
+			return t.Right
 		}
-		return t.right.insert(key)
+		return t.Right.insert(key)
 	default:
 		return t
 	}
@@ -91,19 +91,19 @@ func (t *Tree[T]) delete(key T) *Tree[T] {
 
 	switch cmp := t.cmp(key, *t.Value()); {
 	case cmp < 0:
-		t.left = t.left.delete(key)
+		t.Left = t.Left.delete(key)
 	case cmp > 0:
-		t.right = t.right.delete(key)
+		t.Right = t.Right.delete(key)
 	default:
-		if t.left == nil && t.right == nil {
+		if t.Left == nil && t.Right == nil {
 			t = nil
-		} else if t.left == nil {
-			t = t.right
-		} else if t.right == nil {
-			t = t.left
+		} else if t.Left == nil {
+			t = t.Right
+		} else if t.Right == nil {
+			t = t.Left
 		} else {
-			t.val = t.right.Min().Value()
-			t.right = t.right.delete(*t.Value())
+			t.Val = t.Right.Min().Value()
+			t.Right = t.Right.delete(*t.Value())
 		}
 	}
 
@@ -111,21 +111,21 @@ func (t *Tree[T]) delete(key T) *Tree[T] {
 }
 
 func (t *Tree[T]) Min() *Tree[T] {
-	if t.left == nil {
+	if t.Left == nil {
 		return t
 	}
 
-	return t.left.Min()
+	return t.Left.Min()
 }
 
 func (t *Tree[T]) Max() *Tree[T] {
-	if t.right == nil {
+	if t.Right == nil {
 		return t
 	}
 
-	return t.right.Max()
+	return t.Right.Max()
 }
 
 func (t *Tree[T]) Value() *T {
-	return t.val
+	return t.Val
 }
