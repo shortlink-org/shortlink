2b654a716a8c26d85dbe48935e0c596e722ae0bc
Login Victor <batazor111@gmail.com>
2022-08-09 20:31:53 +0300

    docker-compose: refactoring structure folders
    Signed-off-by: Login Victor <batazor111@gmail.com>

--- ops/Makefile/k8s/gitlab.mk
+++ ops/Makefile/k8s/gitlab.mk
@@ -9,7 +9,7 @@ gitlab-minikube: ## Install GitLab to minikube
       --create-namespace=true \
 	  --set global.hosts.domain=172.17.0.2.nip.io \
  	  --set global.hosts.externalIP=172.17.0.2 \
-	  -f ops/docker-compose/tooling/gitlab/helm-value.yaml
+	  -f ops/docker-compose/tooling/services/gitlab/helm-value.yaml
 	@kubectl -n ${GITLAB_NAMESPACE} get secret gitlab-wildcard-tls-ca -ojsonpath='{.data.cfssl_ca}' | base64 --decode > gitlab.172.17.0.2.nip.io.ca.pem
 	@kubectl -n ${GITLAB_NAMESPACE} get secret gitlab-gitlab-initial-root-password -ojsonpath='{.data.password}' | base64 --decode ; echo " - your password"
 
