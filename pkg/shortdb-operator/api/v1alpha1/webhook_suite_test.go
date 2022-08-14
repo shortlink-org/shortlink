7a9a9adf571cbf43bd3895251684f3e6d470baaa
Login Victor <batazor@evrone.com>
2022-08-14 13:15:30 +0300

    fix version deps
    Signed-off-by: Login Victor <batazor@evrone.com>

--- pkg/shortdb-operator/api/v1alpha1/webhook_suite_test.go
+++ pkg/shortdb-operator/api/v1alpha1/webhook_suite_test.go
@@ -60,7 +60,7 @@ func TestAPIs(t *testing.T) {
 var _ = BeforeSuite(func() {
 	logf.SetLogger(zap.New(zap.WriteTo(GinkgoWriter), zap.UseDevMode(true)))
 
-	ctx, cancel = context.WithCancel(context.TODO())
+	ctx, cancel = context.WithCancel(context.Background())
 
 	By("bootstrapping test environment")
 	testEnv = &envtest.Environment{
