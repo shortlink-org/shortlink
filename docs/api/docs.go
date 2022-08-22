e1c2e12a0bdb11bd63318965ada1624dd94faac6
Login Victor <batazor@evrone.com>
2022-08-22 03:32:17 +0300

    shortlink-api: improve generate swagger of docs
    Signed-off-by: Login Victor <batazor@evrone.com>

--- docs/api/docs.go
+++ docs/api/docs.go
@@ -44,6 +44,66 @@ const docTemplate = `{
                         }
                     }
                 }
+            },
+            "put": {
+                "description": "Update link",
+                "consumes": [
+                    "application/json"
+                ],
+                "produces": [
+                    "application/json"
+                ],
+                "summary": "Update link",
+                "operationId": "update-link",
+                "parameters": [
+                    {
+                        "description": "Link",
+                        "name": "link",
+                        "in": "body",
+                        "required": true,
+                        "schema": {
+                            "$ref": "#/definitions/v1.UpdateRequest"
+                        }
+                    }
+                ],
+                "responses": {
+                    "200": {
+                        "description": "OK",
+                        "schema": {
+                            "$ref": "#/definitions/v1.UpdateResponse"
+                        }
+                    }
+                }
+            },
+            "post": {
+                "description": "Add link",
+                "consumes": [
+                    "application/json"
+                ],
+                "produces": [
+                    "application/json"
+                ],
+                "summary": "Add link",
+                "operationId": "add-link",
+                "parameters": [
+                    {
+                        "description": "Link",
+                        "name": "link",
+                        "in": "body",
+                        "required": true,
+                        "schema": {
+                            "$ref": "#/definitions/v1.AddRequest"
+                        }
+                    }
+                ],
+                "responses": {
+                    "200": {
+                        "description": "OK",
+                        "schema": {
+                            "$ref": "#/definitions/v1.AddResponse"
+                        }
+                    }
+                }
             }
         },
         "/links/{hash}": {
@@ -57,6 +117,15 @@ const docTemplate = `{
                 ],
                 "summary": "Get link",
                 "operationId": "get-link",
+                "parameters": [
+                    {
+                        "type": "string",
+                        "description": "Hash",
+                        "name": "hash",
+                        "in": "path",
+                        "required": true
+                    }
+                ],
                 "responses": {
                     "200": {
                         "description": "OK",
@@ -76,6 +145,15 @@ const docTemplate = `{
                 ],
                 "summary": "Delete link",
                 "operationId": "delete-link",
+                "parameters": [
+                    {
+                        "type": "string",
+                        "description": "Hash",
+                        "name": "hash",
+                        "in": "path",
+                        "required": true
+                    }
+                ],
                 "responses": {
                     "200": {
                         "description": ""
@@ -85,28 +163,7 @@ const docTemplate = `{
         }
     },
     "definitions": {
-        "timestamppb.Timestamp": {
-            "type": "object",
-            "properties": {
-                "nanos": {
-                    "description": "Non-negative fractions of a second at nanosecond resolution. Negative\nsecond values with fractions must still have non-negative nanos values\nthat count forward in time. Must be from 0 to 999,999,999\ninclusive.",
-                    "type": "integer"
-                },
-                "seconds": {
-                    "description": "Represents seconds of UTC time since Unix epoch\n1970-01-01T00:00:00Z. Must be from 0001-01-01T00:00:00Z to\n9999-12-31T23:59:59Z inclusive.",
-                    "type": "integer"
-                }
-            }
-        },
-        "v1.GetResponse": {
-            "type": "object",
-            "properties": {
-                "link": {
-                    "$ref": "#/definitions/v1.Link"
-                }
-            }
-        },
-        "v1.Link": {
+        "github.com_batazor_shortlink_internal_services_link_domain_link_v1.Link": {
             "type": "object",
             "properties": {
                 "created_at": {
@@ -131,13 +188,56 @@ const docTemplate = `{
                 }
             }
         },
+        "github.com_batazor_shortlink_internal_services_link_infrastructure_rpc_link_v1.Link": {
+            "type": "object",
+            "properties": {
+                "LinkServiceServer": {}
+            }
+        },
+        "timestamppb.Timestamp": {
+            "type": "object",
+            "properties": {
+                "nanos": {
+                    "description": "Non-negative fractions of a second at nanosecond resolution. Negative\nsecond values with fractions must still have non-negative nanos values\nthat count forward in time. Must be from 0 to 999,999,999\ninclusive.",
+                    "type": "integer"
+                },
+                "seconds": {
+                    "description": "Represents seconds of UTC time since Unix epoch\n1970-01-01T00:00:00Z. Must be from 0001-01-01T00:00:00Z to\n9999-12-31T23:59:59Z inclusive.",
+                    "type": "integer"
+                }
+            }
+        },
+        "v1.AddRequest": {
+            "type": "object",
+            "properties": {
+                "link": {
+                    "$ref": "#/definitions/github.com_batazor_shortlink_internal_services_link_domain_link_v1.Link"
+                }
+            }
+        },
+        "v1.AddResponse": {
+            "type": "object",
+            "properties": {
+                "link": {
+                    "$ref": "#/definitions/github.com_batazor_shortlink_internal_services_link_domain_link_v1.Link"
+                }
+            }
+        },
+        "v1.GetResponse": {
+            "type": "object",
+            "properties": {
+                "link": {
+                    "$ref": "#/definitions/github.com_batazor_shortlink_internal_services_link_domain_link_v1.Link"
+                }
+            }
+        },
         "v1.Links": {
             "type": "object",
             "properties": {
                 "link": {
                     "type": "array",
                     "items": {
-                        "$ref": "#/definitions/v1.Link"
+                        "$ref": "#/definitions/github.com_batazor_shortlink_internal_services_link_infrastructure_rpc_link_v1.Link"
                     }
                 }
             }
@@ -149,6 +249,22 @@ const docTemplate = `{
                     "$ref": "#/definitions/v1.Links"
                 }
             }
+        },
+        "v1.UpdateRequest": {
+            "type": "object",
+            "properties": {
+                "link": {
+                    "$ref": "#/definitions/github.com_batazor_shortlink_internal_services_link_domain_link_v1.Link"
+                }
+            }
+        },
+        "v1.UpdateResponse": {
+            "type": "object",
+            "properties": {
+                "link": {
+                    "$ref": "#/definitions/github.com_batazor_shortlink_internal_services_link_domain_link_v1.Link"
+                }
+            }
         }
     }
 }`
@@ -158,7 +274,7 @@ var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
 	Host:             "localhost:7070",
 	BasePath:         "/api",
-	Schemes:          []string{},
+	Schemes:          []string{"http", "https"},
 	Title:            "Shortlink API",
 	Description:      "Shortlink API",
 	InfoInstanceName: "swagger",
