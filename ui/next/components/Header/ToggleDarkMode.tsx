a0b4401f7aed0642a0830c9c5ea11f5c46011b73
Login Victor <batazor@evrone.com>
2022-09-05 17:52:05 +0300

    next-ui: add dark mode for material-ui
    Signed-off-by: Login Victor <batazor@evrone.com>

--- ui/next/components/Header/ToggleDarkMode.tsx
+++ ui/next/components/Header/ToggleDarkMode.tsx
@@ -1,16 +1,27 @@
 import MoonIcon from "@heroicons/react/24/solid/MoonIcon";
 import SunIcon from "@heroicons/react/24/solid/SunIcon";
-import { useTheme } from 'next-themes'
-import {useState, useEffect} from "react";
+import { useTheme as nextUseTheme } from 'next-themes'
+import { useState, useContext, useEffect } from "react";
+import { useTheme } from "@mui/material/styles"
+import ColorModeContext from "../../theme/ColorModeContext";
 
 const ToggleDarkMode = () => {
   // @ts-ignore
-  const {systemTheme , theme, setTheme} = useTheme ()
+  const {systemTheme , theme, setTheme} = nextUseTheme()
   const [mounted, setMounted] = useState(false);
 
   useEffect(() =>{
     setMounted(true);
   },[])
+
+  // @ts-ignore
+  const { darkMode, setDarkMode } = useContext(ColorModeContext);
+
+  // @ts-ignore
+  const onClick = e => {
+    setDarkMode(!darkMode)
+    setTheme(e)
+  }
 
   const renderThemeChanger= () => {
     if (!mounted) return null;
@@ -19,13 +30,13 @@ const ToggleDarkMode = () => {
 
     if (currentTheme ==="dark") {
       return (
-        <SunIcon className="w-10 h-10 text-yellow-500 " role="button" onClick={() => setTheme('light')} />
+        <SunIcon className="w-10 h-10 text-yellow-500 " role="button" onClick={() => onClick('light')} />
       )
     }
 
     else {
       return (
-        <MoonIcon className="w-10 h-10 text-gray-900 " role="button" onClick={() => setTheme('dark')} />
+        <MoonIcon className="w-10 h-10 text-gray-900 " role="button" onClick={() => onClick('dark')} />
       )
     }
   }
