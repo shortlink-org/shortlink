c9dfd06a8df6550d5903b59aac04bf04c2c3a539
Login Victor <batazor@evrone.com>
2022-09-13 12:11:54 +0300

    next: improve landing page
    Signed-off-by: Login Victor <batazor@evrone.com>

--- ui/next/components/Landing/feature.tsx
+++ ui/next/components/Landing/feature.tsx
@@ -43,12 +43,11 @@ export default function Feature() {
         <div className="flex items-center flex-wrap mb-20">
           <div className="w-full md:w-1/2">
             <h4 className="text-3xl text-gray-800 font-bold mb-3">
-              Exercise Metrics
+              URL Shortener
             </h4>
             <p className="text-gray-600 mb-8">
-              Our Smart Health Monitoring Wristwatch is able to capture you
-              vitals while you exercise. You can create different category of
-              exercises and can track your vitals on the go.
+              Free custom URL Shortener with many features that gives you better quality for links shortening.
+              We do not display ads during direct redirecting to the original url.
             </p>
           </div>
           <div className="w-full md:w-1/2">
@@ -68,7 +67,7 @@ export default function Feature() {
           <div className="w-full md:w-1/2 pl-10">
             <h4 className="text-3xl text-gray-800 font-bold mb-3">Reporting</h4>
             <p className="text-gray-600 mb-8">
-              Our Smart Health Monitoring Wristwatch can generate a
+              Our shortlink service can generate a
               comprehensive report on your vitals depending on your settings
               either daily, weekly, monthly, quarterly or yearly.
             </p>
@@ -78,9 +77,11 @@ export default function Feature() {
           <div className="w-full md:w-1/2">
             <h4 className="text-3xl text-gray-800 font-bold mb-3">Syncing</h4>
             <p className="text-gray-600 mb-8">
-              Our Smart Health Monitoring Wristwatch allows you to sync data
+              Our shortlink service allows you to sync data
               across all your mobile devices whether iOS, Android or Windows OS
               and also to your laptop whether MacOS, GNU/LInux or Windows OS.
+
+              This allows you to access your data from anywhere at any time.
             </p>
           </div>
           <div className="w-full md:w-1/2">
