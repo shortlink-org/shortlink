e39c1aef670817a92930208aa33f01f4e0bc306b
Login Victor <batazor@evrone.com>
2022-09-18 23:47:45 +0300

    kafka-to-elasticsearch: improve
    Signed-off-by: Login Victor <batazor@evrone.com>

--- internal/services/knative/sync-search/kafka-to-elasticsearch/kafka-to-elasticsearch.go
+++ internal/services/knative/sync-search/kafka-to-elasticsearch/kafka-to-elasticsearch.go
@@ -1,12 +1,83 @@
 package main
 
 import (
+	"context"
+	"fmt"
+	"log"
 	"net/http"
+	"os"
+
+	"github.com/cloudevents/sdk-go/observability/opencensus/v2/client"
+	cloudevents "github.com/cloudevents/sdk-go/v2"
+	cehttp "github.com/cloudevents/sdk-go/v2/protocol/http"
+	"github.com/elastic/go-elasticsearch/v8"
+	"go.uber.org/zap"
+	"knative.dev/pkg/tracing"
+	"knative.dev/pkg/tracing/config"
 )
 
+type Service struct {
+	elastic *elasticsearch.Client
+}
+
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		_, _ = w.Write([]byte("Hello World!"))
+	ctx := context.Background()
+
+	// Init elastic client
+	elasticClient, err := elasticsearch.NewClient(elasticsearch.Config{
+		Addresses: []string{
+			"http://shortlink-master.elasticsearch:9200",
+		},
 	})
-	_ = http.ListenAndServe(":8080", nil)
+
+	service := &Service{
+		elastic: elasticClient,
+	}
+
+	c, err := client.NewClientHTTP(
+		[]cehttp.Option{cehttp.WithMiddleware(healthzMiddleware)}, nil,
+	)
+	if err != nil {
+		log.Fatal("Failed to create client, ", err)
+	}
+
+	conf, err := config.JSONToTracingConfig(os.Getenv("K_CONFIG_TRACING"))
+	if err != nil {
+		log.Fatal("Failed to read tracing config, using the on-op default: %v", err)
+	}
+
+	tracer, err := tracing.SetupPublishingWithStaticConfig(zap.L().Sugar(), "", conf)
+	if err != nil {
+		log.Fatal("Failed to initialize tracing: %v ", err)
+	}
+	defer func(ctx context.Context) {
+		_ = tracer.Shutdown(ctx)
+	}(ctx)
+
+	err = c.StartReceiver(ctx, service.display)
+	if err != nil {
+		log.Fatal("Error during receiver's runtime: ", err)
+	}
+}
+
+// display prints the given Event in a human-readable format.
+func (s *Service) display(event cloudevents.Event) {
+	fmt.Printf("☁️  cloudevents.Event\n%s", event)
+
+	// send event to elastic
+	s.elastic.Index("shortlink.event.link.new", nil)
+}
+
+// HTTP path of the health endpoint used for probing the service.
+const healthzPath = "/healthz"
+
+// healthzMiddleware is a cehttp.Middleware which exposes a health endpoint.
+func healthzMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		if req.RequestURI == healthzPath {
+			w.WriteHeader(http.StatusNoContent)
+		} else {
+			next.ServeHTTP(w, req)
+		}
+	})
 }
