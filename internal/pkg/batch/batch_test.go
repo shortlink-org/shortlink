fc4509d97d26dd1c393fc724dd49c33ef36a017b
Victor Login <batazor111@gmail.com>
2022-12-08 13:41:59 +0300

    pkg: batch: improve test
    Signed-off-by: Victor Login <batazor111@gmail.com>

--- internal/pkg/batch/batch_test.go
+++ internal/pkg/batch/batch_test.go
@@ -10,11 +10,15 @@ import (
 	"time"
 
 	"github.com/stretchr/testify/assert"
+	"go.uber.org/goleak"
 )
 
-//func TestMain(m *testing.M) {
-//	goleak.VerifyTestMain(m)
-//}
+func TestMain(m *testing.M) {
+	goleak.VerifyTestMain(
+		m,
+		goleak.IgnoreTopFunction("github.com/batazor/shortlink/internal/pkg/batch.(*Config).Run"),
+	)
+}
 
 func TestNew(t *testing.T) {
 	t.Run("Create new a batch", func(t *testing.T) {
@@ -52,42 +56,41 @@ func TestNew(t *testing.T) {
 		wg.Wait()
 	})
 
-	// TODO: Fix test with context.Done
-	//t.Run("Check close context", func(t *testing.T) {
-	//	// Add events
-	//	wg := sync.WaitGroup{}
-	//
-	//	aggrCB := func(args []*Item) interface{} {
-	//		// Get string
-	//		for _, item := range args {
-	//			time.Sleep(time.Microsecond * 100) // Emulate long work
-	//
-	//			item.CB <- item.Item.(string)
-	//		}
-	//
-	//		return nil
-	//	}
-	//
-	//	ctx := context.Background()
-	//	ctx, cancel := context.WithCancel(ctx)
-	//	cancel()
-	//
-	//	request := []string{"A", "B", "C", "D"}
-	//
-	//	b, err := New(ctx, aggrCB)
-	//	assert.Nil(t, err)
-	//
-	//	go b.Run(ctx)
-	//
-	//	for key := range request {
-	//		wg.Add(1)
-	//		res, err := b.Push(request[key])
-	//		assert.Nil(t, err)
-	//		go func() {
-	//			assert.Equal(t, <-res, "ctx close")
-	//			wg.Done()
-	//		}()
-	//	}
-	//	wg.Wait()
-	//})
+	t.Run("Check close context", func(t *testing.T) {
+		// Add events
+		wg := sync.WaitGroup{}
+
+		aggrCB := func(args []*Item) interface{} {
+			// Get string
+			for _, item := range args {
+				time.Sleep(time.Microsecond * 100) // Emulate long work
+
+				item.CB <- item.Item.(string)
+			}
+
+			return nil
+		}
+
+		ctx := context.Background()
+		ctx, cancel := context.WithCancel(ctx)
+		cancel()
+
+		request := []string{"A", "B", "C", "D"}
+
+		b, err := New(ctx, aggrCB)
+		assert.Nil(t, err)
+
+		go b.Run(ctx)
+
+		for key := range request {
+			wg.Add(1)
+			res, err := b.Push(request[key])
+			assert.Nil(t, err)
+			go func() {
+				assert.Equal(t, <-res, "ctx close")
+				wg.Done()
+			}()
+		}
+		wg.Wait()
+	})
 }
