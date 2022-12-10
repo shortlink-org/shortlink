10d7e6c1f473afceb9414a11561c174430f05917
Victor Login <batazor111@gmail.com>
2022-12-10 14:58:08 +0300

    http_server: use http.TimeoutHandler
    Signed-off-by: Victor Login <batazor111@gmail.com>

--- pkg/http/server/server.go
+++ pkg/http/server/server.go
@@ -11,13 +11,13 @@ import (
 func New(ctx context.Context, handler http.Handler, config Config) *http.Server {
 	return &http.Server{
 		Addr:    fmt.Sprintf(":%d", config.Port),
-		Handler: handler,
+		Handler: http.TimeoutHandler(handler, config.Timeout, fmt.Sprintf(`{"error": "%s"}`, TimeoutMessage)),
 		BaseContext: func(_ net.Listener) context.Context {
 			return ctx
 		},
 
-		ReadTimeout:  1 * time.Second,                 // the maximum duration for reading the entire request, including the body
-		WriteTimeout: config.Timeout + 30*time.Second, // the maximum duration before timing out writes of the response
+		ReadTimeout:  1 * time.Second,                // the maximum duration for reading the entire request, including the body
+		WriteTimeout: config.Timeout + 5*time.Second, // the maximum duration before timing out writes of the response
 		// the maximum amount of time to wait for the next request when keep-alive is enabled
 		IdleTimeout: 30 * time.Second, // nolint:gomnd
 		// the amount of time allowed to read request headers
