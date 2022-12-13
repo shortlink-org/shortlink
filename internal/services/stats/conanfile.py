c14612d529e6b900105fffc90d78eac656258f4c
Victor Login <batazor111@gmail.com>
2022-12-14 02:50:06 +0300

    stats: add prometheus exporter :9090/metrics
    Signed-off-by: Victor Login <batazor111@gmail.com>

--- internal/services/stats/conanfile.py
+++ internal/services/stats/conanfile.py
@@ -1,6 +1,5 @@
 from conans import ConanFile
 
-
 class MyConanFile(ConanFile):
-    requires = "cli11/1.9.1", "fmt/7.1.3", "prometheus-cpp/1.0.0"
+    requires = "cli11/1.9.1", "fmt/7.1.3", "prometheus-cpp/1.1.0"
     generators = "cmake", "BazelDeps", "BazelToolchain"
