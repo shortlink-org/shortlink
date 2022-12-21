360c5b0a0b0c865ccb6ae267671d591238b2c2fe
Victor Login <batazor111@gmail.com>
2022-12-21 04:21:21 +0300

    referral: use flask and opentelemetry
    Signed-off-by: Victor Login <batazor111@gmail.com>

--- internal/services/referral/main.py
+++ internal/services/referral/main.py
@@ -1,1 +1,21 @@
-print('Hello, World!')
+from flask import Flask
+from opentelemetry import metrics
+from opentelemetry import trace
+
+# Create a Tracer
+tracer = trace.get_tracer(__name__)
+meter = metrics.get_meter(__name__)
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def handle_get_request():
+    # Start a new span
+    with tracer.start_as_current_span("handle_get_request"):
+        # Return a response
+        return "Hello, world!"
+
+
+if __name__ == '__main__':
+    app.run()
