e3010ebccd6dce88a531501ffe1ad6acbea8bb10
Victor Login <batazor111@gmail.com>
2022-12-24 02:43:52 +0300

    ui: dep upd
    Signed-off-by: Victor Login <batazor111@gmail.com>

--- ui/ui-kit/rollup.config.js
+++ ui/ui-kit/rollup.config.js
@@ -1,11 +1,12 @@
-import { nodeResolve } from "@rollup/plugin-node-resolve"
+import {nodeResolve} from "@rollup/plugin-node-resolve"
 import commonjs from "@rollup/plugin-commonjs"
 import typescript from "@rollup/plugin-typescript"
-import { terser } from "rollup-plugin-terser"
+import {terser} from "rollup-plugin-terser"
 import peerDepsExternal from 'rollup-plugin-peer-deps-external'
 import postcss from 'rollup-plugin-postcss'
 import dts from "rollup-plugin-dts"
 import babel from 'rollup-plugin-babel'
+import css from 'rollup-plugin-css-only'
 
 const packageJson = require("./package.json")
 
@@ -39,9 +40,10 @@ export default [
     ],
     external: ["react", "react-dom", "styled-components", "next-themes", "@emotion/cache", "@mui/material"]
   },
-  // {material
-  //   input: "dist/esm/index.d.ts",
-  //   output: [{ file: "dist/index.d.ts", format: "esm" }],
-  //   plugins: [dts()],
-  // },
+  {
+    input: "dist/esm/index.d.ts",
+    output: [{ file: "dist/index.d.ts", format: "es" }],
+    plugins: [css(), dts.default()],
+    external: [/\.css$/u],
+  },
 ];
