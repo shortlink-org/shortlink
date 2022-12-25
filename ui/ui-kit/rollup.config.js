b124fb40767a62c919f4cf6309ec0359e7271a0d
Victor Login <batazor111@gmail.com>
2022-12-25 03:16:01 +0300

    ui-kit: fix build
    Signed-off-by: Victor Login <batazor111@gmail.com>

--- ui/ui-kit/rollup.config.js
+++ ui/ui-kit/rollup.config.js
@@ -1,22 +1,34 @@
 import {nodeResolve} from "@rollup/plugin-node-resolve"
 import commonjs from "@rollup/plugin-commonjs"
-import typescript from "@rollup/plugin-typescript"
-import {terser} from "rollup-plugin-terser"
+import ts from "@rollup/plugin-typescript"
+import typescript from 'typescript'
 import peerDepsExternal from 'rollup-plugin-peer-deps-external'
 import postcss from 'rollup-plugin-postcss'
 import dts from "rollup-plugin-dts"
-import babel from 'rollup-plugin-babel'
+import filesize from 'rollup-plugin-filesize'
 
 const packageJson = require("./package.json")
+
+const globals = {
+  react: 'React',
+  'react-dom': 'ReactDOM',
+  classnames: 'classNames',
+  'prop-types': 'PropTypes',
+  '@emotion/styled': 'emStyled',
+  '@emotion/react': 'react',
+  'react/jsx-runtime': 'jsxRuntime',
+}
 
 export default [
   {
     input: "src/index.ts",
     output: [
       {
+        name: packageJson.name,
         file: packageJson.main,
-        format: "cjs",
+        format: "umd",
         sourcemap: true,
+        globals,
       },
       {
         file: packageJson.module,
@@ -25,21 +37,36 @@ export default [
       },
     ],
     plugins: [
-      nodeResolve(),
-      peerDepsExternal(),
+      peerDepsExternal(), // Preferably set as first plugin.
       postcss({
-        extract: true,
+        extract: 'index.css',
+        autoModules: true,
+        include: 'src/**/*.css',
+        extensions: ['.css'],
+        plugins: [],
       }),
-      babel({
-        exclude: 'node_modules/**',
+      nodeResolve(),
+      ts({
+        typescript,
+        tsconfig: './tsconfig.json',
+        noEmitOnError: false,
+        // declaration: true,
+        // declarationDir: './build',
       }),
-      commonjs(),
-      typescript({ tsconfig: "./tsconfig.json" }),
-      terser()
+      commonjs({
+        include: 'node_modules/**',
+      }),
+      filesize(),
+      // babel({
+      //   exclude: 'node_modules/**',
+      // }),
+      // terser()
     ],
-    external: ["react", "react-dom", "styled-components", "next-themes", "@emotion/cache", "@mui/material"]
+    // external: ["react", "react-dom", "styled-components", "next-themes", "@emotion/cache", "@mui/material"]
+    external: ['react', 'react-dom', 'prop-types', 'styled-components', "@mui/material"],
   },
   {
+    name: packageJson.name,
     input: "dist/esm/types/index.d.ts",
     output: [{ file: "dist/index.d.ts", format: "es" }],
     plugins: [
@@ -50,11 +77,12 @@ export default [
         extensions: ['.css'],
         plugins: [],
       }),
-      dts({
+      dts.default({
         compilerOptions: {
           baseUrl: 'src',
         },
       }),
     ],
+    external: ['react', 'react-dom', 'prop-types', 'styled-components', "@mui/material"],
   },
 ];
