7b67757fb19f815f9e84e605960312dc239f7081
Victor Login <batazor111@gmail.com>
2023-01-11 22:47:51 +0600

    docker: enable SBOM
    Signed-off-by: Victor Login <batazor111@gmail.com>

--- ops/Makefile/docker.mk
+++ ops/Makefile/docker.mk
@@ -27,6 +27,9 @@ docker_build:
 	@docker buildx build --platform=linux/amd64 \
 		--force-rm \
 		--opt attest:provenance=mode=min,inline-only=false \
+		--opt build-arg:BUILDKIT_SBOM_SCAN_STAGE=true \
+		--opt build-arg:BUILDKIT_SBOM_SCAN_CONTEXT=true \
+		--opt attest:sbom= \
 		--push \
 		-t ${CI_REGISTRY_IMAGE}-$(SERVICE):${CI_COMMIT_TAG} \
 		-f ops/dockerfile/$(SERVICE).Dockerfile .
