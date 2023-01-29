fbabcbc1dc820b730fa6b4414c6055af24395510
Victor Login <batazor@evrone.com>
2023-01-30 00:32:46 +0600

    shortink-proxy: fix ready path
    Signed-off-by: Victor Login <batazor@evrone.com>

--- internal/services/proxy/src/app.ts
+++ internal/services/proxy/src/app.ts
@@ -31,6 +31,7 @@ const APP_CONFIGURED = SERVER.build();
 async function onHealthCheck () {
   // checks if the system is healthy, like the db connection is live
   // resolves, if health, rejects if not
+  return true
 }
 
 // graceful shutdown and Kubernetes readiness/liveness checks
