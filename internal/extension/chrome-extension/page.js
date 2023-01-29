1165a378509c9899a41b61fc1704dbf4d402351e
Victor Login <batazor@evrone.com>
2023-01-29 22:00:21 +0600

    chrome-extension: improve
    Signed-off-by: Victor Login <batazor@evrone.com>

--- internal/extension/chrome-extension/page.js
+++ internal/extension/chrome-extension/page.js
@@ -13,8 +13,9 @@ function addURLToContainer(links) {
         var item = document.createElement("li");
 
         // Set the item's inner HTML to include the link and text
-        var newElement = document.createElement('a');
-        newElement.innerHTML = "<a href='" + links[i].href + "'>" + links[i].text + "</a>";
+        var newLinkElement = document.createElement('a');
+        newLinkElement = newLinkElement.setAttribute('href', links[i].href);
+        newLinkElement = newElement.innerHTML = links[i].text;
         item.appendChild(newElement);
 
         // Add the item to the list
