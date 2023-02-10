2b2852adf4b0a726d294337ee44b6b0fa55dbbb6
Victor Login <batazor@evrone.com>
2023-02-10 14:50:22 +0600

    ui-kit: fix
    Signed-off-by: Victor Login <batazor@evrone.com>

--- ui/ui-kit/rollup.config.js
+++ ui/ui-kit/rollup.config.js
@@ -65,24 +65,24 @@ export default [
     // external: ["react", "react-dom", "styled-components", "next-themes", "@emotion/cache", "@mui/material"]
     external: ['react', 'react-dom', 'prop-types', 'styled-components', "@mui/material"],
   },
-  {
-    name: packageJson.name,
-    input: "dist/esm/types/index.d.ts",
-    output: [{ file: "dist/index.d.ts", format: "es" }],
-    plugins: [
-      postcss({
-        extract: 'styles.css', // this will generate a specific file not being used, but we need this part of code
-        autoModules: true,
-        include: '**/*.css',
-        extensions: ['.css'],
-        plugins: [],
-      }),
-      dts.default({
-        compilerOptions: {
-          baseUrl: 'src',
-        },
-      }),
-    ],
-    external: ['react', 'react-dom', 'prop-types', 'styled-components', "@mui/material"],
-  },
+  // {
+  //   name: packageJson.name,
+  //   input: "dist/esm/types/index.d.ts",
+  //   output: [{ file: "dist/index.d.ts", format: "es" }],
+  //   plugins: [
+  //     postcss({
+  //       extract: 'styles.css', // this will generate a specific file not being used, but we need this part of code
+  //       autoModules: true,
+  //       include: '**/*.css',
+  //       extensions: ['.css'],
+  //       plugins: [],
+  //     }),
+  //     dts.default({
+  //       compilerOptions: {
+  //         baseUrl: 'src',
+  //       },
+  //     }),
+  //   ],
+  //   external: ['react', 'react-dom', 'prop-types', 'styled-components', "@mui/material"],
+  // },
 ];
