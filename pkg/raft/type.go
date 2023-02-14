5d7a2382b0dab10d5cbf5371b13adf544f8c13e9
Victor Login <batazor@evrone.com>
2023-02-15 04:56:51 +0600

    raft: improve base interfaces
    Signed-off-by: Victor Login <batazor@evrone.com>

--- pkg/raft/type.go
+++ pkg/raft/type.go
@@ -4,21 +4,64 @@ import (
 	"time"
 )
 
+// Raft represents a distributed consensus algorithm.
 type Raft interface {
+	// Start starts the Raft node.
+	// It returns an error if the node fails to start.
+	Start() error
+
+	// Stop stops the Raft node.
+	// It returns an error if the node fails to stop.
+	Stop() error
+
+	// AddNode adds a new node to the cluster.
+	// It takes a node ID and returns an error if the ID is invalid or
+	// the node fails to be added to the cluster.
+	AddNode(nodeID uint64) error
+
+	// RemoveNode removes a node from the cluster.
+	// It takes a node ID and returns an error if the ID is invalid or
+	// the node fails to be removed from the cluster.
+	RemoveNode(nodeID uint64) error
+
+	// Propose proposes a new command to the Raft cluster.
+	// It takes a node ID and a command value, which can be any type.
+	// The method returns an error if the ID is invalid or the proposal
+	// fails to be processed by the specified node.
+	Propose(nodeID uint64, command interface{}) error
+}
+
+// RaftRole represents the role of a Raft node.
+type RaftRole uint8
+
+const (
+	Follower RaftRole = iota
+	Candidate
+	Leader
+)
+
+// RaftClient represents a client that interacts with a Raft cluster.
+type RaftClient interface {
+	// Connect connects the client to the Raft cluster.
+	// It takes a Config struct and returns an error if the connection fails.
 	Connect(config Config) error
+
+	// Close closes the connection to the Raft cluster.
+	// It returns an error if the connection fails to be closed.
+	Close() error
 }
 
-type Vote struct {
-	Timeout  time.Duration
-	NextVote time.Time
-}
-
+// Config represents the configuration for a Raft client.
 type Config struct {
-	Name   string
-	Weight int
-	URI    string
+	// NodeAddrs is a list of addresses of the nodes in the Raft cluster.
+	NodeAddrs []string
 
-	Vote Vote
+	// Timeout is the timeout duration for requests to the Raft nodes.
+	Timeout time.Duration
 
-	Status string
+	// RetryLimit is the number of times to retry failed requests to the Raft nodes.
+	RetryLimit int
+
+	// RetryInterval is the interval duration between retries of failed requests to the Raft nodes.
+	RetryInterval time.Duration
 }
