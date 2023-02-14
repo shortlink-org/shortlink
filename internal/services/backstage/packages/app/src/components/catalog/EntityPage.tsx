c7e31861220652f20f88efb3bfb289d131831afd
Victor Login <batazor@evrone.com>
2023-02-15 00:09:48 +0600

    backstage: add plugin lighthouse
    Signed-off-by: Victor Login <batazor@evrone.com>

--- internal/services/backstage/packages/app/src/components/catalog/EntityPage.tsx
+++ internal/services/backstage/packages/app/src/components/catalog/EntityPage.tsx
@@ -56,6 +56,7 @@ import {
   EntityArgoCDOverviewCard,
   isArgocdAvailable
 } from '@roadiehq/backstage-plugin-argo-cd';
+import { EntityLighthouseContent } from '@backstage/plugin-lighthouse';
 
 import { TechDocsAddons } from '@backstage/plugin-techdocs-react';
 import { ReportIssue } from '@backstage/plugin-techdocs-module-addons-contrib';
@@ -164,6 +165,10 @@ const serviceEntityPage = (
   <EntityLayout>
     <EntityLayout.Route path="/" title="Overview">
       {overviewContent}
+    </EntityLayout.Route>
+
+    <EntityLayout.Route path="/lighthouse" title="Lighthouse">
+      <EntityLighthouseContent />
     </EntityLayout.Route>
 
     <EntityLayout.Route path="/kubernetes" title="Kubernetes">
