d0b3b5dbc3601e0b8c6c8e48db0b6ebdef63c131
Victor Login <batazor@evrone.com>
2023-02-23 10:25:43 +0100

    support: add deps for opentelemetry
    Signed-off-by: Victor Login <batazor@evrone.com>

---
+++ internal/services/support/autoload_sdk.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+putenv('OTEL_PHP_AUTOLOAD_ENABLED=true');
+putenv('OTEL_TRACES_EXPORTER=otlp');
+putenv('OTEL_EXPORTER_OTLP_PROTOCOL=grpc');
+putenv('OTEL_METRICS_EXPORTER=otlp');
+putenv('OTEL_EXPORTER_OTLP_METRICS_PROTOCOL=grpc');
+putenv('OTEL_EXPORTER_OTLP_ENDPOINT=http://grafana-tempo.grafana:4317');
+putenv('OTEL_PHP_TRACES_PROCESSOR=batch');
+putenv('OTEL_PROPAGATORS=b3,baggage,tracecontext');
+
+echo 'autoloading SDK example starting...' . PHP_EOL;
+
+// Composer autoloader will execute SDK/_autoload.php which will register global instrumentation from environment configuration
+require dirname(__DIR__) . '/vendor/autoload.php';
+
+$instrumentation = new \OpenTelemetry\API\Common\Instrumentation\CachedInstrumentation('shortlink-support');
+
+$instrumentation->tracer()->spanBuilder('root')->startSpan()->end();
+$instrumentation->meter()->createCounter('cnt')->add(1);
+
+echo 'Finished!' . PHP_EOL;
