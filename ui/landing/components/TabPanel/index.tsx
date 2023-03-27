76cc2b07c382d5423dfad5b7b9701cbaf2e79398
Victor Login <batazor@evrone.com>
2023-03-27 13:46:10 +0200

    landing: prettier
    Signed-off-by: Victor Login <batazor@evrone.com>

--- ui/landing/components/TabPanel/index.tsx
+++ ui/landing/components/TabPanel/index.tsx
@@ -20,11 +20,7 @@ function TabPanel(props: TabPanelProps) {
       aria-labelledby={`full-width-tab-${index}`}
       {...other}
     >
-      {value === index && (
-        <Box sx={{ p: 3 }}>
-          {children}
-        </Box>
-      )}
+      {value === index && <Box sx={{ p: 3 }}>{children}</Box>}
     </div>
   )
 }
