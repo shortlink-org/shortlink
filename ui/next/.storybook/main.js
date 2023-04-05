f34f501fb7c600bfd590db3fa515f93b7c4a83b3
Victor Login <batazor@evrone.com>
2023-04-05 15:00:59 +0200

    ui-kit: migrate to storybook v7
    Signed-off-by: Victor Login <batazor@evrone.com>

--- ui/next/.storybook/main.js
+++ ui/next/.storybook/main.js
@@ -1,15 +1,11 @@
 module.exports = {
-  stories: [
-    '../stories/**/*.stories.mdx',
-    '../stories/**/*.stories.@(js|jsx|ts|tsx)',
-  ],
-  addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-interactions',
-  ],
+  stories: ['../stories/**/*.stories.mdx', '../stories/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: ['@storybook/addon-links', '@storybook/addon-styling', '@storybook/addon-essentials', '@storybook/addon-interactions'],
   framework: '@storybook/react',
   core: {
-    builder: '@storybook/builder-webpack5',
+    builder: '@storybook/builder-webpack5'
   },
-}
+  docs: {
+    autodocs: true
+  }
+};
