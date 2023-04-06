55bd85c6091e000e7bef639de21e796158ae0ccd
Victor Login <batazor@evrone.com>
2023-04-06 14:19:35 +0200

    pkg: improve kafka - add more options for setting and add healthcheck
    Signed-off-by: Victor Login <batazor@evrone.com>

--- internal/pkg/mq/v1/kafka/kafka.go
+++ internal/pkg/mq/v1/kafka/kafka.go
@@ -2,8 +2,11 @@ package kafka
 
 import (
 	"context"
+	"fmt"
+	"time"
 
 	"github.com/Shopify/sarama"
+	"github.com/heptiolabs/healthcheck"
 	"github.com/spf13/viper"
 
 	"github.com/shortlink-org/shortlink/internal/pkg/mq/v1/query"
@@ -25,7 +28,10 @@ func (mq *Kafka) Init(ctx context.Context) error {
 	var err error
 
 	// Set configuration
-	config := mq.setConfig()
+	config, err := mq.setConfig()
+	if err != nil {
+		return err
+	}
 
 	if mq.client, err = sarama.NewClient(mq.Config.URI, config); err != nil {
 		return err
@@ -40,6 +46,15 @@ func (mq *Kafka) Init(ctx context.Context) error {
 	if mq.consumer, err = sarama.NewConsumerGroupFromClient(mq.ConsumerGroup, mq.client); err != nil {
 		return err
 	}
+
+	// run cron for check connection
+	healthcheck.AsyncWithContext(ctx, func() error {
+		if len(mq.client.Brokers()) > 0 {
+			return nil
+		}
+
+		return fmt.Errorf("kafka connection error")
+	}, 5*time.Second)
 
 	return nil
 }
@@ -93,10 +108,12 @@ func (mq *Kafka) UnSubscribe(target string) error {
 }
 
 // setConfig - Construct a new Sarama configuration.
-func (mq *Kafka) setConfig() *sarama.Config {
+func (mq *Kafka) setConfig() (*sarama.Config, error) {
 	viper.AutomaticEnv()
-	viper.SetDefault("MQ_KAFKA_URI", "localhost:9092")                           // Kafka URI
-	viper.SetDefault("MQ_KAFKA_CONSUMER_GROUP", viper.GetString("SERVICE_NAME")) // Kafka consumer group
+	viper.SetDefault("MQ_KAFKA_URI", "localhost:9092")                                                         // Kafka URI
+	viper.SetDefault("MQ_KAFKA_CONSUMER_GROUP", viper.GetString("SERVICE_NAME"))                               // Kafka consumer group
+	viper.SetDefault("MQ_KAFKA_CONSUMER_GROUP_PARTITION_ASSIGNMENT_STRATEGY", sarama.RangeBalanceStrategyName) // Consumer group partition assignment strategy (range, roundrobin, sticky)
+	viper.SetDefault("MQ_KAFKA_CONSUMER_GROUP_OFFSET", sarama.OffsetNewest)                                    // Kafka consumer consume initial offset from oldest
 
 	mq.Config = &Config{
 		URI: []string{
@@ -107,17 +124,29 @@ func (mq *Kafka) setConfig() *sarama.Config {
 
 	// sarama config
 	config := sarama.NewConfig()
-
 	config.ClientID = viper.GetString("SERVICE_NAME")
+
+	switch viper.GetString("MQ_KAFKA_CONSUMER_GROUP_PARTITION_ASSIGNMENT_STRATEGY") {
+	case sarama.StickyBalanceStrategyName:
+		config.Consumer.Group.Rebalance.GroupStrategies = []sarama.BalanceStrategy{sarama.NewBalanceStrategySticky()}
+	case sarama.RoundRobinBalanceStrategyName:
+		config.Consumer.Group.Rebalance.GroupStrategies = []sarama.BalanceStrategy{sarama.NewBalanceStrategyRoundRobin()}
+	case sarama.RangeBalanceStrategyName:
+		config.Consumer.Group.Rebalance.GroupStrategies = []sarama.BalanceStrategy{sarama.NewBalanceStrategyRange()}
+	default:
+		return nil, fmt.Errorf("Unrecognized consumer group partition assignor: %s", viper.GetString("MQ_KAFKA_CONSUMER_GROUP_PARTITION_ASSIGNMENT_STRATEGY"))
+	}
+
+	config.Consumer.Offsets.Initial = viper.GetInt64("MQ_KAFKA_CONSUMER_GROUP_OFFSET")
 
 	config.Producer.Partitioner = sarama.NewRandomPartitioner
 	config.Producer.RequiredAcks = sarama.WaitForAll
 	config.Producer.Retry.Max = 5
 	config.Producer.Return.Successes = true
 	config.Producer.Compression = sarama.CompressionSnappy
-	config.Version = sarama.V2_5_0_0
+	config.Version = sarama.MaxVersion
 
 	config.Consumer.Return.Errors = true
 
-	return config
+	return config, nil
 }
