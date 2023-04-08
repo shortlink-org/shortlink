9b7f34a92893bbe05b936d9c55f00cd56fd7e55d
Victor Login <batazor@evrone.com>
2023-04-09 01:34:03 +0200

    go: improve http observability
    Signed-off-by: Victor Login <batazor@evrone.com>

--- pkg/http/client/client.go
+++ pkg/http/client/client.go
@@ -4,10 +4,14 @@ import (
 	"net/http"
 
 	"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
+	"go.opentelemetry.io/otel"
 )
 
 func New() *http.Client {
 	return &http.Client{
-		Transport: otelhttp.NewTransport(http.DefaultTransport),
+		Transport: otelhttp.NewTransport(
+			http.DefaultTransport,
+			otelhttp.WithPropagators(otel.GetTextMapPropagator()),
+		),
 	}
 }
