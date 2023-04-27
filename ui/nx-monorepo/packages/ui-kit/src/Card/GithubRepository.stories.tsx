5b79d959383505e34cc2d48fc91938d37f0fd721
Victor Login <batazor@evrone.com>
2023-04-27 18:20:51 +0200

    ui-kit: add card GitHubRepository
    Signed-off-by: Victor Login <batazor@evrone.com>

---
+++ ui/nx-monorepo/packages/ui-kit/src/Card/GithubRepository.stories.tsx
@@ -0,0 +1,24 @@
+// @ts-ignore
+import React, {useContext, useEffect, useState} from 'react'
+import { StoryObj, Meta, Preview } from '@storybook/react'
+
+import GithubRepository  from './GithubRepository'
+
+const meta: Meta<any> = {
+  title: 'UI/GithubRepository',
+  component: GithubRepository,
+  tags: ['autodocs'],
+}
+
+export default meta
+
+const Template = (args) => (
+  <GithubRepository
+    title={'GitHub Repository'}
+    url={'https://github.com/shortlink-org/shortlink'}
+    {...args}
+  />
+)
+
+export const Default = Template.bind({});
+Default.args = {};
