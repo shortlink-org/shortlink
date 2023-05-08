94bef42b2553731999c5c272cd80f6fe05e61489
Victor Login <batazor@evrone.com>
2023-05-08 20:17:08 +0200

    ui: upd
    Signed-off-by: Victor Login <batazor@evrone.com>

--- ui/nx-monorepo/packages/next/components/PushNotificationLayout.tsx
+++ ui/nx-monorepo/packages/next/components/PushNotificationLayout.tsx
@@ -9,29 +9,29 @@ import { useRouter } from 'next/router'
 
 function PushNotificationLayout({ children }) {
   const router = useRouter()
-  // useEffect(() => {
-  //   setToken()
-  //
-  //   // Event listener that listens for the push notification event in the background
-  //   if ('serviceWorker' in navigator) {
-  //     navigator.serviceWorker.addEventListener('message', (event) => {
-  //       console.log('event for the service worker', event)
-  //     })
-  //   }
-  //
-  //   // Calls the getMessage() function if the token is there
-  //   async function setToken() {
-  //     try {
-  //       const token = await firebaseCloudMessaging.init()
-  //       if (token) {
-  //         console.log('token', token)
-  //         getMessage()
-  //       }
-  //     } catch (error) {
-  //       console.log(error)
-  //     }
-  //   }
-  // })
+  useEffect(() => {
+    setToken()
+
+    // Event listener that listens for the push notification event in the background
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker.addEventListener('message', (event) => {
+        console.log('event for the service worker', event)
+      })
+    }
+
+    // Calls the getMessage() function if the token is there
+    async function setToken() {
+      try {
+        const token = await firebaseCloudMessaging.init()
+        if (token) {
+          console.log('token', token)
+          getMessage()
+        }
+      } catch (error) {
+        console.log(error)
+      }
+    }
+  })
 
   // Handles the click function on the toast showing push notification
   const handleClickPushNotification = (url) => {
