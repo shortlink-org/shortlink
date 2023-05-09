bca1d0feeb76ec5563e1f0a486a7311137c0a7d4
Victor Login <batazor@evrone.com>
2023-05-09 15:16:30 +0200

    csi: refactoring makefile
    Signed-off-by: Victor Login <batazor@evrone.com>

--- internal/services/csi/README.md
+++ internal/services/csi/README.md
@@ -8,15 +8,23 @@ Driver for Container Storage Interface (CSI) for Kubernetes.
 
 ### shortctl docs - [link](./docs/shortctl-csi.md)
 
+### Gettings started
+
+We use Makefile for build and deploy.
+
+```bash
+$> make help # show help message with all commands and targets
+```
+
 ### Docs
 
 - [What is The Container Storage Interface (CSI)?](https://www.architecting.it/blog/container-storage-interface/)
 - [Official docs](https://kubernetes-csi.github.io/docs/)
 - [[RU] Понимаем Container Storage Interface (в Kubernetes и не только)](https://habr.com/ru/company/flant/blog/424211/)
 
-### Example
+### References
 
 - [csi-driver-host-path](https://github.com/kubernetes-csi/csi-driver-host-path) - a sample (non-production) CSI Driver that creates a local directory as a volume on a single node
 - [csi-digitalocean](https://github.com/digitalocean/csi-digitalocean) - A Container Storage Interface (CSI) Driver for DigitalOcean Block Storage
 
-**csi-digitalocean** has a more friendly year. 
+**csi-digitalocean** has a more friendly. 
--- ops/Makefile/docker.mk
+++ ops/Makefile/docker.mk
@@ -20,30 +20,105 @@ CI_REGISTRY_IMAGE := batazor/${PROJECT_NAME}
 CI_COMMIT_TAG := latest
 SHORTLINK_SERVICES := api billing bot csi landing link logger metadata notify proxy ui-next
 
-docker: docker-login docker-build ## docker login > build > push
-
+# DOCKER TASKS =========================================================================================================
 docker-login: ## Docker login
 	@echo docker login as ${DOCKER_USERNAME}
 	@echo ${DOCKER_PASSWORD} | docker login -u ${DOCKER_USERNAME} --password-stdin
-
-docker_build:
-	@echo "Building ${CI_REGISTRY_IMAGE}-$(SERVICE):${CI_COMMIT_TAG}"
-	@docker buildx build --platform=linux/amd64 \
-		--provenance=true \
-		--sbom=true \
-		--force-rm \
-		--push \
-		-t ${CI_REGISTRY_IMAGE}-$(SERVICE):${CI_COMMIT_TAG} \
-		-f ops/dockerfile/$(SERVICE).Dockerfile .
-	@docker push ${CI_REGISTRY_IMAGE}-$(SERVICE):${CI_COMMIT_TAG}
-	@docker rmi ${CI_REGISTRY_IMAGE}-$(SERVICE):${CI_COMMIT_TAG}
-
-docker-build: ## Build the container
-	for i in $(SHORTLINK_SERVICES); do \
-		make docker_build SERVICE=$$i; \
-  	done
 
 ### Helpers ============================================================================================================
 
 docker_ip: ## View docker ip and container name
 	@docker ps -q | xargs docker inspect --format "{{range .NetworkSettings.Networks}}{{print .IPAddress}} {{end}}{{.Name}}"
+
+
+# APPLICATION TASKS ====================================================================================================
+dep: ## Install dependencies for this project
+	# install protoc addons
+	@go install github.com/swaggo/swag/cmd/swag@latest
+	@go install github.com/srikrsna/protoc-gen-gotag@latest
+	@go install moul.io/protoc-gen-gotemplate@latest
+	@go install github.com/cloudflare/cfssl/cmd/...@latest
+	@go install golang.org/x/tools/cmd/goimports@latest
+
+	# for NodeJS
+	@npm install -g grpc-tools grpc_tools_node_protoc_ts ts-protoc-gen protoc-gen-ts
+
+	# install wire
+	@go install github.com/google/wire/cmd/wire@latest
+
+	#i18n
+	@go install golang.org/x/text/cmd/gotext@latest
+
+export CURRENT_UID=$(id -u):$(id -g)
+
+dev: ## Run for development mode
+	@COMPOSE_PROFILES=dns,observability,gateway docker compose \
+		-f docker-compose.yaml \
+		-f ops/docker-compose/tooling/services/coredns/coredns.yaml \
+		-f ops/docker-compose/database/redis/redis.yaml \
+		-f ops/docker-compose/tooling/observability/grafana/grafana-tempo.yaml \
+		up -d --remove-orphans --build
+
+run: ## Run this project in docker compose
+	@docker compose \
+		-f docker-compose.yaml \
+		-f ops/docker-compose/tooling/services/coredns/coredns.yaml \
+		-f ops/docker-compose/tooling/observability/fluent-bit/fluent-bit.yaml \
+		-f ops/docker-compose/gateway/traefik/traefik.yaml \
+		-f ops/docker-compose/application/auth/kratos/kratos.yaml \
+		-f ops/docker-compose/application/auth/hydra/hydra.yaml \
+		-f ops/docker-compose/application/auth/keto/keto.yaml \
+		-f ops/docker-compose/application/api/api.yaml \
+		-f ops/docker-compose/application/metadata/metadata.yaml \
+		-f ops/docker-compose/application/logger/logger.yaml \
+		-f ops/docker-compose/application/ui-next/ui-next.yaml \
+		-f ops/docker-compose/database/mongo.yaml \
+		-f ops/docker-compose/tooling/observability/prometheus/prometheus.yaml \
+		-f ops/docker-compose/tooling/observability/grafana/grafana.yaml \
+		-f ops/docker-compose/tooling/observability/grafana/grafana-loki.yaml \
+		-f ops/docker-compose/tooling/observability/grafana/grafana-tempo.yaml \
+		-f ops/docker-compose/tooling/observability/grafana/grafana-phlare.yaml \
+		-f ops/docker-compose/tooling/observability/grafana/grafana-oncall.yaml \
+		-f ops/docker-compose/mq/rabbitmq.yaml \
+		up -d --remove-orphans
+
+down: confirm ## Down docker compose
+	@docker compose \
+		-f docker-compose.yaml \
+		-f ops/docker-compose/tooling/services/coredns/coredns.yaml \
+		-f ops/docker-compose/tooling/saas/airflow/airflow.yaml \
+		-f ops/docker-compose/tooling/saas/nifi/nifi.yaml \
+		-f ops/docker-compose/tooling/observability/grafana/grafana.yaml \
+		-f ops/docker-compose/tooling/observability/grafana/grafana-tempo.yaml \
+		-f ops/docker-compose/tooling/observability/prometheus/prometheus.yaml \
+		-f ops/docker-compose/tooling/observability/fluent-bit/fluent-bit.yaml \
+		-f ops/docker-compose/tooling/observability/pyroscope/pyroscope.yaml \
+		-f ops/docker-compose/gateway/caddy/caddy.yaml \
+		-f ops/docker-compose/gateway/nginx/nginx.yaml \
+		-f ops/docker-compose/gateway/traefik/traefik.yaml \
+		-f ops/docker-compose/application/auth/keycloak/keycloak.yaml \
+		-f ops/docker-compose/application/auth/kratos/kratos.yaml \
+		-f ops/docker-compose/application/auth/hydra/hydra.yaml \
+		-f ops/docker-compose/application/auth/keto/keto.yaml \
+		-f ops/docker-compose/application/api/api.yaml \
+		-f ops/docker-compose/application/metadata/metadata.yaml \
+		-f ops/docker-compose/application/logger/logger.yaml \
+		-f ops/docker-compose/application/support/support.yaml \
+		-f ops/docker-compose/application/ui-next/ui-next.yaml \
+		-f ops/docker-compose/database/mongo/mongo.yaml \
+		-f ops/docker-compose/database/redis/redis.yaml \
+		-f ops/docker-compose/database/dragonfly/dragonfly.yaml \
+		-f ops/docker-compose/database/postgres/patroni.yaml \
+		-f ops/docker-compose/database/postgres/postgres.yaml \
+		-f ops/docker-compose/database/elasticsearch/elasticsearch.yaml \
+		-f ops/docker-compose/database/neo4j/neo4j.yaml \
+		-f ops/docker-compose/mq/rabbitmq/rabbitmq.yaml \
+		-f ops/docker-compose/mq/kafka/zookeeper.yaml \
+		-f ops/docker-compose/mq/kafka/kafka.yaml \
+		-f ops/docker-compose/mq/kafka/kafka-schema-registry.yaml \
+		-f ops/docker-compose/mq/kafka/kafka-connect.yaml \
+		-f ops/docker-compose/mq/kafka/kafka-connector-postgres.yaml \
+		-f ops/docker-compose/mq/kafka/kafka-connector-elasticsearch.yaml \
+		-f ops/docker-compose/mq/nats/nats.yaml \
+	down --remove-orphans
+	@docker network prune -f
