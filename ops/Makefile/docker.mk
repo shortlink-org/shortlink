92bcb495b4e381648a80683d7428dae36a995f28
Victor Login <batazor@evrone.com>
2023-05-11 00:28:52 +0200

    ui-next: fix url to auth api
    Signed-off-by: Victor Login <batazor@evrone.com>

--- ops/Makefile/docker.mk
+++ ops/Makefile/docker.mk
@@ -57,6 +57,7 @@ dev: ## Run for development mode
 		-f ops/docker-compose/tooling/services/coredns/coredns.yaml \
 		-f ops/docker-compose/database/redis/redis.yaml \
 		-f ops/docker-compose/tooling/observability/grafana/grafana-tempo.yaml \
+		-f ops/docker-compose/application/auth/kratos/kratos.yaml \
 		up -d --remove-orphans --build
 
 run: ## Run this project in docker compose
