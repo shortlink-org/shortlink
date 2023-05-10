aa48a16f776426d4bac61b7bf5a90bed475e55aa
Victor Login <batazor@evrone.com>
2023-05-11 00:28:52 +0200

    ui-next: fix url to auth api
    Signed-off-by: Victor Login <batazor@evrone.com>

--- ui/nx-monorepo/packages/next/pkg/sdk/index.ts
+++ ui/nx-monorepo/packages/next/pkg/sdk/index.ts
@@ -1,6 +1,6 @@
 import { Configuration, FrontendApi } from '@ory/client'
 
-const NEXT_PUBLIC_API_URI = `${process.env.NEXT_PUBLIC_API_URI}/api/auth`
+const NEXT_PUBLIC_API_URI = `${process.env.NEXT_PUBLIC_API_URI || ""}/api/auth`
 
 const ory = new FrontendApi(
   new Configuration({
