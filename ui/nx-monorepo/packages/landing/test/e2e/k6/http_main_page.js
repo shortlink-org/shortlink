97c00aecb039aea6ef9515668c24e58a0df1c405
Victor Login <batazor@evrone.com>
2023-05-21 15:57:40 +0200

    testkube: improve test for shortlink-landing
    Signed-off-by: Victor Login <batazor@evrone.com>

---
+++ ui/nx-monorepo/packages/landing/test/e2e/k6/http_main_page.js
@@ -0,0 +1,9 @@
+import { check } from 'k6'
+import http from 'k6/http'
+
+export default function () {
+  const res = http.get('shortlink-landing.shortlink:8080')
+  check(res, {
+    'is status 200': (r) => r.status === 200,
+  })
+}
