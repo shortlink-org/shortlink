ae89d6ac3454bb4377c74f877e4b9ff88fdb564b
Victor Login <batazor@evrone.com>
2023-05-22 06:59:03 +0200

    testkube: improve test
    Signed-off-by: Victor Login <batazor@evrone.com>

--- ui/nx-monorepo/packages/landing/test/e2e/k6/http_main_page.js
+++ ui/nx-monorepo/packages/landing/test/e2e/k6/http_main_page.js
@@ -1,8 +1,23 @@
 import { check } from 'k6'
 import http from 'k6/http'
 
-export default function () {
-  const res = http.get('http://shortlink-landing.shortlink:8080')
+// eslint-disable-next-line import/no-mutable-exports
+export let options = {
+  ext: {
+    loadimpact: {
+      // eslint-disable-next-line no-undef
+      projectID: __ENV.K6_PROJECT_ID,
+      // Test runs with the same name groups test runs together
+      name: 'HTTP main page',
+    },
+  },
+}
+
+export default () => {
+  // eslint-disable-next-line no-undef
+  const TARGET_HOSTNAME = __ENV.TARGET_HOSTNAME || 'localhost:3001'
+
+  const res = http.get(`http://${TARGET_HOSTNAME}/`)
   check(res, {
     'is status 200': (r) => r.status === 200,
   })
