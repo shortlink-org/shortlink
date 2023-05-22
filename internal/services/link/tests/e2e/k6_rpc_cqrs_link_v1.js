e47964acb66bbfb5211daf4983d1ba88ee0cd661
Victor Login <batazor@evrone.com>
2023-05-22 09:58:35 +0200

    shortlink-link: add test for cqrs
    Signed-off-by: Victor Login <batazor@evrone.com>

---
+++ internal/services/link/tests/e2e/k6_rpc_cqrs_link_v1.js
@@ -0,0 +1,96 @@
+import grpc from 'k6/net/grpc';
+import { check } from 'k6';
+
+// eslint-disable-next-line import/no-mutable-exports
+export let options = {
+  ext: {
+    loadimpact: {
+      // eslint-disable-next-line no-undef
+      projectID: __ENV.K6_PROJECT_ID,
+      // Test runs with the same name groups test runs together
+      name: 'gRPC LinkService CQRS Link',
+    },
+  },
+}
+
+// Load the gRPC client
+const client = new grpc.Client();
+
+// Load the proto files
+client.load(['definitions'], [
+  './infrastructure/rpc/cqrs/link/v1/link.proto',
+  './infrastructure/rpc/cqrs/link/v1/link_query.proto'
+]);
+
+export default () => {
+  // eslint-disable-next-line no-undef
+  const TARGET_HOSTNAME = __ENV.TARGET_HOSTNAME || 'localhost:50051'
+
+  // Connect to the gRPC server
+  client.connect(TARGET_HOSTNAME, { timeout: "5s" });
+
+  // Test the Add method
+  let addRequest = {
+    link: {
+      url: 'google.com',
+      describe: 'yourDescription',
+    },
+  };
+  let addResponse = client.invoke('LinkCommandService/Add', addRequest);
+  check(addResponse, {
+    'Add call succeeded': (r) => r && r.status === grpc.StatusOK,
+  });
+
+  // Extract the hash from the AddResponse
+  let hash = addResponse.message.link.hash;
+
+  // Test the Get method
+  let getRequest = { hash: hash };
+  let getResponse = client.invoke('LinkQueryService/Get', getRequest);
+  check(getResponse, {
+    'Get call succeeded': (r) => r && r.status === grpc.StatusOK,
+  });
+
+  // Test the List method
+  let listRequest = { filter: hash };
+  let listResponse = client.invoke('LinkQueryService/List', listRequest);
+  check(listResponse, {
+    'List call succeeded': (r) => r && r.status === grpc.StatusOK,
+  });
+
+  // Test the Update method
+  let updateRequest = {
+    link: {
+      url: 'google.com',
+      hash: hash,
+      describe: 'yourUpdatedDescription',
+      // Add timestamps as needed
+    },
+  };
+  let updateResponse = client.invoke('LinkCommandService/Update', updateRequest);
+  check(updateResponse, {
+    'Update call succeeded': (r) => r && r.status === grpc.StatusOK,
+  });
+
+  // Test the Delete method
+  let deleteRequest = { hash: hash };
+  let deleteResponse = client.invoke('LinkCommandService/Delete', deleteRequest);
+  check(deleteResponse, {
+    'Delete call succeeded': (r) => r && r.status === grpc.StatusOK,
+  });
+
+  // Negative test: Get method should fail after Delete
+  getResponse = client.invoke('LinkQueryService/Get', getRequest);
+  check(getResponse, {
+    'Get call failed after delete': (r) => r && r.status !== grpc.StatusOK,
+  });
+
+  // Negative test: List method should not return the deleted link
+  listResponse = client.invoke('LinkQueryService/List', listRequest);
+  check(listResponse, {
+    'List call did not return deleted link': (r) => r && r.status === grpc.StatusOK && !r.message.links.link.some(link => link.hash === hash),
+  });
+
+  // Close the connection at the end of the test
+  client.close();
+};
