2466e88eae4ce821ea5623d02b48b086e20be2de
Victor Login <batazor@evrone.com>
2023-06-01 20:16:40 +0200

    referral: use 0.0.0.0
    Signed-off-by: Victor Login <batazor@evrone.com>

--- internal/services/referral/src/__main__.py
+++ internal/services/referral/src/__main__.py
@@ -22,7 +22,7 @@ def main(
     """Application entrypoint."""
     register_routes(app, referral_service, use_service)
     logger.info("Starting application")
-    app.run(host='localhost', port=8000, debug=True, use_reloader=False)
+    app.run(host='0.0.0.0', port=8000, debug=True, use_reloader=False)
 
 
 if __name__ == '__main__':
