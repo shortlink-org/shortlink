efc99a387fc22ce44bdfdb75dd0b6273268c6dd9
Victor Login <batazor@evrone.com>
2023-07-02 18:32:16 +0200

    concurrency: add rate limiter
    Signed-off-by: Victor Login <batazor@evrone.com>

---
+++ internal/pkg/concurrency/rate_limiter/rate_limiter.go
@@ -0,0 +1,66 @@
+package rate_limiter
+
+import (
+	"context"
+	"sync"
+	"time"
+)
+
+type RateLimiter struct {
+	mu  sync.Mutex
+	ctx context.Context
+
+	limit   int64
+	limiter chan struct{}
+	ticker  *time.Ticker
+}
+
+func New(ctx context.Context, limit int64, interval time.Duration) (*RateLimiter, error) {
+	ticker := time.NewTicker(interval)
+
+	rl := &RateLimiter{
+		ctx:     ctx,
+		limit:   limit,
+		limiter: make(chan struct{}, limit),
+		ticker:  ticker,
+	}
+
+	go rl.refill()
+
+	return rl, nil
+}
+
+func (r *RateLimiter) Wait() error {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+
+	select {
+	case <-r.limiter:
+		return nil
+	case <-r.ctx.Done():
+		return r.ctx.Err()
+	}
+}
+
+// refill refills tokens periodically
+func (r *RateLimiter) refill() {
+	for {
+		select {
+		case <-r.ticker.C:
+			for i := int64(0); i < r.limit; i++ {
+				select {
+				case r.limiter <- struct{}{}:
+				default:
+				}
+			}
+		case <-r.ctx.Done():
+			return
+		}
+	}
+}
+
+func (r *RateLimiter) Close() {
+	r.ticker.Stop()
+
+	close(r.limiter)
+}
---
+++ internal/pkg/concurrency/rate_limiter/rate_limiter_test.go
@@ -0,0 +1,42 @@
+package rate_limiter
+
+import (
+	"context"
+	"sync/atomic"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+	"golang.org/x/sync/errgroup"
+)
+
+func TestRateLimiter(t *testing.T) {
+	sum := int64(0)
+
+	// Use a context with a timeout to prevent test from running indefinitely
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	rl, err := New(ctx, 100, 5*time.Millisecond)
+	require.NoError(t, err)
+
+	defer rl.Close()
+
+	var wg errgroup.Group
+
+	for i := 0; i < 10000; i++ {
+		wg.Go(func() error {
+			err := rl.Wait()
+			if err != nil {
+				return err
+			}
+
+			atomic.AddInt64(&sum, 1)
+			return nil
+		})
+	}
+
+	err = wg.Wait()
+	require.NoError(t, err)
+	require.Equal(t, int64(10000), atomic.LoadInt64(&sum))
+}
