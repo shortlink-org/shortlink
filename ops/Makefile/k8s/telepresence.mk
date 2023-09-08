5d8663ced334fd605081b89f8f0b7872aac9ec81
Victor Login <batazor@evrone.com>
2023-09-08 12:52:39 +0200

    ops: setting
    Signed-off-by: Victor Login <batazor@evrone.com>

--- ops/Makefile/k8s/telepresence.mk
+++ ops/Makefile/k8s/telepresence.mk
@@ -2,7 +2,7 @@
 telepresence-up: ## Starts the local daemon and connects Telepresence to your cluster and installs the Traffic Manager if it is missing.
 	-mkdir -p /Users/$USER/Library/Caches/telepresence
 	@telepresence helm upgrade
-	@echo "enter: $> telepresence connect"
+	@echo "enter: \$> telepresence connect"
 
 telepresence-down: ## Quits the local daemon, stopping all intercepts and outbound traffic to the cluster
 	@telepresence quit
