f8a306bb8085bbfbf5a41db23c1facc59b03f5a2
Victor Login <batazor@evrone.com>
2023-11-01 10:43:28 +0100

    upd deps
    Signed-off-by: Victor Login <batazor@evrone.com>

--- internal/services/bff-web/infrastructure/http/api/api.go
+++ internal/services/bff-web/infrastructure/http/api/api.go
@@ -1,3 +1,3 @@
-//go:generate go run github.com/deepmap/oapi-codegen/cmd/oapi-codegen --config=config.yaml api.yaml
+//go:generate go run github.com/deepmap/oapi-codegen/v2/cmd/oapi-codegen --config=config.yaml api.yaml
 
 package api
