1cbeee09550486c1c157530269deacf4b6d55126
Victor Login <batazor@evrone.com>
2023-11-01 22:42:36 +0100

    gafana-tempo: use vParquet3
    Signed-off-by: Victor Login <batazor@evrone.com>

--- ops/Makefile/docker.mk
+++ ops/Makefile/docker.mk
@@ -53,8 +53,7 @@ dev: ## Run for development mode
 	@COMPOSE_PROFILES=dns,observability,gateway docker compose \
 		-f docker-compose.yaml \
 		-f ops/docker-compose/tooling/services/coredns/coredns.yaml \
-		-f ops/docker-compose/gateway/traefik/traefik.yaml \
-		-f ops/docker-compose/database/redis/redis.yaml \
+		-f ops/docker-compose/tooling/observability/grafana/grafana-tempo.yaml \
 		up -d --remove-orphans --build
 
 watch: ## Run for development mode with watch
