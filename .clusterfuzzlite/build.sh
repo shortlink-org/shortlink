580d99860a888bb42b65b7a83681651d8034ea2f
Victor Login <batazor@evrone.com>
2023-11-11 15:10:03 +0100

    ClusterFuzzLite: setting
    Signed-off-by: Victor Login <batazor@evrone.com>

--- .clusterfuzzlite/build.sh
+++ .clusterfuzzlite/build.sh
@@ -1,9 +1,5 @@
 #!/bin/bash -eu
 
-# These two dependencies cause build issues and are not used by oss-fuzz:
-rm -r sqlparser
-rm -r parser
-
 go mod init "github.com/shortlink-org/shortlink"
 export FUZZ_ROOT="github.com/shortlink-org/shortlink"
 
