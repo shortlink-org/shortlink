0418ffa97ed65b5750d46c52e636075708ce8f7c
Victor Login <batazor@evrone.com>
2023-11-11 15:12:48 +0100

    ClusterFuzzLite: setting
    Signed-off-by: Victor Login <batazor@evrone.com>

--- .clusterfuzzlite/build.sh
+++ .clusterfuzzlite/build.sh
@@ -1,6 +1,3 @@
 #!/bin/bash -eu
 
-go mod init "github.com/shortlink-org/shortlink"
-export FUZZ_ROOT="github.com/shortlink-org/shortlink"
-
 compile_go_fuzzer github.com/shortlink-org/shortlink/internal/pkg/batch FuzzBatch fuzz_batch
