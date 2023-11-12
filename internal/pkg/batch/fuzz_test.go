50778df8d82e71d30d90cdac05b166c733d76d29
Victor Login <batazor@evrone.com>
2023-11-12 19:00:14 +0100

    pkg/batch: try fuzz test
    Signed-off-by: Victor Login <batazor@evrone.com>

---
+++ internal/pkg/batch/fuzz_test.go
@@ -0,0 +1,48 @@
+//go:build fuzz
+// +build fuzz
+
+package batch
+
+import (
+	"context"
+	"testing"
+	"time"
+)
+
+func FuzzBatch(f *testing.F) {
+	// Adding basic types
+	f.Add([]byte("byte slice"))
+
+	f.Fuzz(func(t *testing.T, input []byte) {
+		// Create a context with a timeout
+		ctx, cancel := context.WithTimeout(context.Background(), 60*time.Second)
+		defer cancel()
+
+		// Define a callback function
+		callback := func(items []*Item) any {
+			// Add assertions here to check the state of items
+			return nil
+		}
+
+		// Initialize the Batch with the callback
+		batch, err := New(ctx, callback)
+		if err != nil {
+			t.Fatalf("Failed to create batch: %v", err)
+		}
+
+		// Push the fuzzed input to the batch
+		callbackChan := batch.Push(input)
+
+		// Optionally, you can wait for the callback to complete
+		select {
+		case <-callbackChan:
+			// Callback completed
+		case <-time.After(60 * time.Second):
+			// Handle timeout if the callback takes too long
+			t.Fatal("Callback timed out")
+		}
+
+		// Stop the batch processing
+		batch.Stop()
+	})
+}
