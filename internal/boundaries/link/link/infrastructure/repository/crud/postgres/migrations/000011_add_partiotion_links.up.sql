8173e856a243789e1967f7aaa4da8abb5646f83d
Victor Login <batazor@evrone.com>
2023-12-23 23:37:32 +0100

    shortlink-link: setting
    Signed-off-by: Victor Login <batazor@evrone.com>

--- internal/boundaries/link/link/infrastructure/repository/crud/postgres/migrations/000011_add_partiotion_links.up.sql
+++ internal/boundaries/link/link/infrastructure/repository/crud/postgres/migrations/000011_add_partiotion_links.up.sql
@@ -1,3 +1,15 @@
+CREATE DOMAIN hash AS TEXT CHECK (length(VALUE) > 0 AND length(VALUE) <= 9);
+
+CREATE DOMAIN link AS jsonb CHECK (
+  CASE jsonb_typeof(value->'url')::text
+    WHEN 'string' THEN
+      length(to_json(value->'url')::text) > 0
+        AND
+      length(to_json(value->'hash')::text) > 0
+    ELSE false
+    END
+);
+
 CREATE TABLE link.links_partitioned_by_created_at
 (
     id         uuid      DEFAULT gen_random_uuid() NOT NULL,
@@ -14,7 +26,7 @@ CREATE TABLE link.links_partitioned_by_created_at
 CREATE SCHEMA IF NOT EXISTS partman;
 CREATE EXTENSION IF NOT EXISTS pg_partman SCHEMA partman;
 
-SELECT partman.create_parent('link.links_partitioned_by_created_at', 'created_at', 'native', 'daily');
+SELECT partman.create_parent('link.links_partitioned_by_created_at', 'created_at', '1 days', 'range');
 UPDATE partman.part_config SET retention = '10 days' WHERE parent_table = 'link.links_partitioned_by_created_at';
 
 SELECT partman.run_maintenance();
