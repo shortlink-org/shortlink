72275a73a2cd5923ccdb4014ccce44b6b1349435
Victor Login <batazor@evrone.com>
2023-12-27 00:31:08 +0100

    speech-to-text: add di and config for gitlab-ci
    Signed-off-by: Victor Login <batazor@evrone.com>

---
+++ internal/boundaries/chat/speech-to-text/cmd/speech-to-text.go
@@ -0,0 +1,40 @@
+/*
+Chat boundary
+
+Speech-to-text (STT) Service
+*/
+package main
+
+import (
+	"os"
+
+	"github.com/spf13/viper"
+
+	stt_di "github.com/shortlink-org/shortlink/internal/boundaries/chat/speech-to-text/di"
+	"github.com/shortlink-org/shortlink/internal/pkg/graceful_shutdown"
+)
+
+func main() {
+	viper.SetDefault("SERVICE_NAME", "speech-to-text")
+
+	// Init a new service
+	service, cleanup, err := stt_di.InitializeSTTService()
+	if err != nil {
+		panic(err)
+	}
+	service.Log.Info("Service initialized")
+
+	defer func() {
+		if r := recover(); r != nil {
+			service.Log.Error(r.(string)) //nolint:forcetypeassert // simple type assertion
+		}
+	}()
+
+	// Handle SIGINT, SIGQUIT and SIGTERM.
+	graceful_shutdown.GracefulShutdown()
+
+	cleanup()
+
+	// Exit Code 143: Graceful Termination (SIGTERM)
+	os.Exit(143) //nolint:gocritic // TODO: research
+}
---
+++ internal/boundaries/chat/speech-to-text/di/wire.go
@@ -0,0 +1,68 @@
+//go:generate wire
+//go:build wireinject
+
+// The build tag makes sure the stub is not built in the final build.
+
+/*
+Speech-to-text Service DI-package
+*/
+package stt_di
+
+import (
+	"github.com/google/wire"
+	"go.opentelemetry.io/otel/trace"
+
+	"github.com/shortlink-org/shortlink/internal/di"
+	"github.com/shortlink-org/shortlink/internal/di/pkg/autoMaxPro"
+	"github.com/shortlink-org/shortlink/internal/di/pkg/config"
+	"github.com/shortlink-org/shortlink/internal/di/pkg/profiling"
+	"github.com/shortlink-org/shortlink/internal/pkg/logger"
+	"github.com/shortlink-org/shortlink/internal/pkg/observability/monitoring"
+)
+
+type STTService struct {
+	// Common
+	Log    logger.Logger
+	Config *config.Config
+
+	// Observability
+	Tracer        trace.TracerProvider
+	Monitoring    *monitoring.Monitoring
+	PprofEndpoint profiling.PprofEndpoint
+	AutoMaxPro    autoMaxPro.AutoMaxPro
+}
+
+// STTService =========================================================================================================
+var STTSet = wire.NewSet(
+	di.DefaultSet,
+
+	NewSTTService,
+)
+
+func NewSTTService(
+	// Common
+	log logger.Logger,
+	config *config.Config,
+
+	// Observability
+	monitoring *monitoring.Monitoring,
+	tracer trace.TracerProvider,
+	pprofHTTP profiling.PprofEndpoint,
+	autoMaxProcsOption autoMaxPro.AutoMaxPro,
+) (*STTService, error) {
+	return &STTService{
+		// Common
+		Log:    log,
+		Config: config,
+
+		// Observability
+		Tracer:        tracer,
+		Monitoring:    monitoring,
+		PprofEndpoint: pprofHTTP,
+		AutoMaxPro:    autoMaxProcsOption,
+	}, nil
+}
+
+func InitializeSTTService() (*STTService, func(), error) {
+	panic(wire.Build(STTSet))
+}
