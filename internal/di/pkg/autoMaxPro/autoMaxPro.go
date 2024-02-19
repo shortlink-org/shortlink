2a078bc730848511e78b8cb32bbd8d134da4b183
Victor Login <batazor111@gmail.com>
2024-02-19 21:11:26 +0100

    automaxpro: improve logs
    Signed-off-by: Victor Login <batazor111@gmail.com>

--- internal/di/pkg/autoMaxPro/autoMaxPro.go
+++ internal/di/pkg/autoMaxPro/autoMaxPro.go
@@ -13,7 +13,7 @@ type AutoMaxPro *string
 // New - Automatically set GOMAXPROCS to match Linux container CPU quota
 func New(log logger.Logger) (AutoMaxPro, func(), error) {
 	undo, err := maxprocs.Set(maxprocs.Logger(func(s string, args ...any) {
-		log.Info(fmt.Sprintf(s, args))
+		log.Info(fmt.Sprintf(s, args...))
 	}))
 	if err != nil {
 		return nil, nil, err
