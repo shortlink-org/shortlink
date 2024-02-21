aede5b25ab732fb1f08fef67b60f9acdcf35485d
Victor Login <batazor111@gmail.com>
2024-02-21 20:07:00 +0100

    gitlab: refactoring CI
    Signed-off-by: Victor Login <batazor111@gmail.com>

--- boundaries/link/link/domain/link/v1/link_fuzz.go
+++ boundaries/link/link/domain/link/v1/link_fuzz.go
@@ -3,13 +3,14 @@
 package v1
 
 func Fuzz(link []byte) int {
-	data := &Link{Url: string(link)}
+	data := &Link{
+		Url: string(link),
+	}
+
 	err := NewURL(data)
-	if err != nil {
+	if err != nil || len(data.Hash) != 7 {
 		return -1
 	}
-	if len(newLink.Hash) != 7 {
-		return -1
-	}
+
 	return 1
 }
