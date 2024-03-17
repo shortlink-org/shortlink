79f9ca9fa57b789ff179d0a9591e137862f23314
Victor Login <batazor111@gmail.com>
2024-03-17 06:46:39 +0100

    ui: refactoring
    Signed-off-by: Victor Login <batazor111@gmail.com>

--- boundaries/ui/nx-monorepo/packages/next/next.config.js
+++ boundaries/ui/nx-monorepo/packages/next/next.config.js
@@ -131,14 +131,6 @@ const NEXT_CONFIG = {
     webVitalsAttribution: ['CLS', 'FCP', 'FID', 'INP', 'LCP', 'TTFB'],
     turbo: {},
     // typedRoutes: true,
-    modularizeImports: {
-      '@mui/material': {
-        transform: '@mui/material/{{member}}',
-      },
-      '@mui/icons-material': {
-        transform: '@mui/icons-material/{{member}}',
-      },
-    },
   },
 }
 
