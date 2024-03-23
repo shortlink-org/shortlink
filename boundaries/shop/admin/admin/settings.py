91e38381ee91efd773667e3898d99eb8c8c8efba
Victor Login <batazor111@gmail.com>
2024-03-23 15:44:36 +0100

    shop: use tailwindcss for admin

--- boundaries/shop/admin/admin/settings.py
+++ boundaries/shop/admin/admin/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'django_admin_tailwind',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
