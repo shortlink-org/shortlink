14bbb04c2300b6bd40565f9ee4ac6fc4c0d7b89f
Victor Login <batazor111@gmail.com>
2024-04-02 01:51:01 +0200

    shop-admin: setting
    Signed-off-by: Victor Login <batazor111@gmail.com>

---
+++ boundaries/shop/admin/src/migration.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+"""Django's command-line utility for administrative tasks."""
+
+import logging
+import os
+import sys
+
+from admin.otel_logging import CustomLogRecord
+from opentelemetry.instrumentation.django import DjangoInstrumentor
+from opentelemetry.instrumentation.requests import RequestsInstrumentor
+
+logging.setLogRecordFactory(CustomLogRecord)
+
+
+def main():
+    """Run administrative tasks."""
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "admin.settings")
+
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError as exc:
+        raise ImportError(
+            "Couldn't import Django. Are you sure it's installed and "
+            "available on your PYTHONPATH environment variable? Did you "
+            "forget to activate a virtual environment?"
+        ) from exc
+    execute_from_command_line(sys.argv)
+
+    DjangoInstrumentor().instrument()
+    RequestsInstrumentor().instrument()
+
+
+if __name__ == "__main__":
+    main()
