72a7ce00010dda05e00b3f7441f5f0a0e069cc26
Victor Login <batazor111@gmail.com>
2024-07-18 11:46:18 +0200

    devops: add cleanup_pods script
    Signed-off-by: Victor Login <batazor111@gmail.com>

---
+++ ops/script/cleanup_pods.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Script to delete pods with specific statuses in all namespaces
+# This script deletes pods with the following statuses: Evicted, Error, Completed, ContainerStatusUnknown
+# It retrieves all namespaces and iterates over each namespace to find and delete the matching pods
+
+# Get all namespaces
+namespaces=$(kubectl get ns --no-headers -o custom-columns=:metadata.name)
+
+# Loop through each namespace
+for ns in $namespaces; do
+  echo "Processing namespace: $ns"
+  kubectl get pod -n $ns --no-headers | grep -E 'Evicted|Error|Completed|ContainerStatusUnknown' | awk '{print $1}' | xargs -r kubectl delete pod -n $ns
+done
