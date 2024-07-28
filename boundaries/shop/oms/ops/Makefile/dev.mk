d68f9d70cbd8bfaa03ed1b8d23f2c577c0438b3b
Victor Login <batazor111@gmail.com>
2024-07-29 00:13:19 +0200

    oms: add env ROOT_DIR to makefile
    Signed-off-by: Victor Login <batazor111@gmail.com>

--- boundaries/shop/oms/ops/Makefile/dev.mk
+++ boundaries/shop/oms/ops/Makefile/dev.mk
@@ -1,17 +1,18 @@
 # INCLUDE ==============================================================================================================
 # Include Makefile
-include $(SELF_DIR)/../../../ops/Makefile/docker.mk
+include $(ROOT_DIR)/ops/Makefile/docker.mk
 
 ### Runnings ===========================================================================================================
 up: ## Run for development mode
 	@COMPOSE_PROFILES=dns,observability,gateway docker compose \
-		-f $(SELF_DIR)/../../../docker-compose.yaml \
-		-f $(SELF_DIR)/../../../ops/docker-compose/tooling/services/coredns/coredns.yaml \
+		-f $(ROOT_DIR)/docker-compose.yaml \
+		-f $(ROOT_DIR)/ops/docker-compose/tooling/services/coredns/coredns.yaml \
+		-f $(ROOT_DIR)/ops/docker-compose/tooling/saas/temporal/temporal.yaml \
 		up -d --remove-orphans --build
 
 down: confirm ## Down docker compose
 	@COMPOSE_PROFILES=dns,observability,gateway docker compose \
-		-f $(SELF_DIR)/../../../docker-compose.yaml \
-		-f $(SELF_DIR)/../../../ops/docker-compose/tooling/services/coredns/coredns.yaml \
+		-f $(ROOT_DIR)/docker-compose.yaml \
+		-f $(ROOT_DIR)/ops/docker-compose/tooling/services/coredns/coredns.yaml \
 	down --remove-orphans
 	@docker network prune -f
