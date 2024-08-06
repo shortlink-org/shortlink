989e5e0d27e4e7b30da524f6f5bef58a401c64ab
Victor Login <batazor111@gmail.com>
2024-08-06 23:14:57 +0200

    temporal: add tracing
    Signed-off-by: Victor Login <batazor111@gmail.com>

--- pkg/di/pkg/temporal/temporal.go
+++ pkg/di/pkg/temporal/temporal.go
@@ -3,6 +3,8 @@ package temporal
 import (
 	"github.com/uber-go/tally/v4/prometheus"
 	"go.temporal.io/sdk/client"
+	"go.temporal.io/sdk/contrib/opentelemetry"
+	"go.temporal.io/sdk/interceptor"
 
 	"github.com/shortlink-org/shortlink/pkg/logger"
 	"github.com/shortlink-org/shortlink/pkg/observability/monitoring"
@@ -19,9 +21,16 @@ func New(log logger.Logger, monitoring *monitoring.Monitoring) (client.Client, e
 		return nil, err
 	}
 
+	// create Interceptor
+	tracingInterceptor, err := opentelemetry.NewTracingInterceptor(opentelemetry.TracerOptions{})
+	if err != nil {
+		return nil, err
+	}
+
 	// Create the client object just once per process
 	c, err := client.Dial(client.Options{
 		MetricsHandler: sdktally.NewMetricsHandler(metrics),
+		Interceptors:   []interceptor.ClientInterceptor{tracingInterceptor},
 	})
 	if err != nil {
 		return nil, err
