0dc4ecbfee78b430f6c93bb581643dcfd95ba9c2
Victor Login <batazor111@gmail.com>
2024-08-10 15:21:39 +0200

    upd deps
    Signed-off-by: Victor Login <batazor111@gmail.com>

--- boundaries/ui-monorepo/packages/landing/postcss.config.js
+++ boundaries/ui-monorepo/packages/landing/postcss.config.js
@@ -1,9 +1,6 @@
 module.exports = {
   plugins: {
-    'postcss-preset-env': {
-      browsers: 'last 2 versions',
-    },
-    '@tailwindcss/postcss': {},
-    autoprefixer: {},
+    // tailwindcss: {},
+    // autoprefixer: {},
   },
 }
