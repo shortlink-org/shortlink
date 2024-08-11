a1892e8e255ab0ebf1331420093f63867bd07195
Victor Login <batazor111@gmail.com>
2024-08-11 22:52:06 +0200

    upd deps
    Signed-off-by: Victor Login <batazor111@gmail.com>

--- boundaries/ui-monorepo/packages/ui-kit/.storybook/main.ts
+++ boundaries/ui-monorepo/packages/ui-kit/.storybook/main.ts
@@ -4,57 +4,46 @@ const config: StorybookConfig = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
     '@storybook/addon-links',
-    '@storybook/addon-essentials'
-    // getAbsolutePath('@storybook/addon-themes'),
-    // {
-    //   name: getAbsolutePath('@storybook/addon-essentials'),
-    //   options: {
-    //     actions: true,
-    //     backgrounds: true,
-    //     controls: true,
-    //     docs: true, // https://github.com/hipstersmoothie/react-docgen-typescript-plugin/issues/83
-    //     viewport: true,
-    //     toolbars: true,
-    //   },
-    // },
-    // getAbsolutePath('@storybook/addon-interactions'),
-    // getAbsolutePath('@storybook/addon-controls'),
-    // {
-    //   name: getAbsolutePath('@storybook/addon-styling-webpack'),
-    //   options: {
-    //     postCss: {
-    //       implementation: require.resolve('postcss'),
-    //     },
-    //     rules: [
-    //       {
-    //         test: /\.css$/,
-    //         sideEffects: true,
-    //         use: [
-    //           require.resolve('style-loader'),
-    //           {
-    //             loader: require.resolve('css-loader'),
-    //             options: {
-    //               // Want to add more CSS Modules options? Read more here: https://github.com/webpack-contrib/css-loader#modules
-    //               modules: {
-    //                 auto: true,
-    //               },
-    //               importLoaders: 1,
-    //             },
-    //           },
-    //           {
-    //             loader: require.resolve('postcss-loader'),
-    //             options: {
-    //               implementation: require.resolve('postcss'),
-    //             },
-    //           },
-    //         ],
-    //       },
-    //     ],
-    //   },
-    // },
-    // getAbsolutePath('@chromatic-com/storybook'),
-    // getAbsolutePath('@storybook/addon-a11y'),
-    // getAbsolutePath('@storybook/addon-coverage'),
+    '@storybook/addon-essentials',
+    '@storybook/addon-themes',
+    '@storybook/addon-interactions',
+    '@storybook/addon-controls',
+    '@chromatic-com/storybook',
+    '@storybook/addon-a11y',
+    '@storybook/addon-coverage',
+    {
+      name: '@storybook/addon-styling-webpack',
+      options: {
+        postCss: {
+          implementation: require.resolve('postcss'),
+        },
+        rules: [
+          {
+            test: /\.css$/,
+            sideEffects: true,
+            use: [
+              require.resolve('style-loader'),
+              {
+                loader: require.resolve('css-loader'),
+                options: {
+                  // Want to add more CSS Modules options? Read more here: https://github.com/webpack-contrib/css-loader#modules
+                  modules: {
+                    auto: true,
+                  },
+                  importLoaders: 1,
+                },
+              },
+              {
+                loader: require.resolve('postcss-loader'),
+                options: {
+                  implementation: require.resolve('postcss'),
+                },
+              },
+            ],
+          },
+        ],
+      },
+    },
   ],
   framework: '@storybook/react-vite',
   features: {},
