e3c7821493ec08440561f13c0c9db542ff9471eb
Victor Login <batazor111@gmail.com>
2024-08-12 22:42:09 +0200

    shop-admin: improve makefile. add docker stack

---
+++ boundaries/shop/admin/ops/Makefile/dev.mk
@@ -0,0 +1,25 @@
+# ADMIN TASKS ==========================================================================================================
+dep: ## Install dependencies
+	# Create a virtual environment at .venv
+	uv venv
+
+	# Install dependencies
+	uv pip install -r pyproject.toml --no-deps
+
+lock: ## Lock dependencies
+	-rm requirements.txt
+	@uv pip compile pyproject.toml --generate-hashes -o requirements.txt --no-deps
+
+run: ## Run server
+	@python src/manage.py runserver
+
+test: ## Run tests
+	@pytest --fixtures tests
+
+lint: ## Run linter
+	@ruff format
+	@ruff check --fix .
+
+# MIGRATION TASKS ======================================================================================================
+migrate: ## Run migrations
+	@python src/migration.py migrate
