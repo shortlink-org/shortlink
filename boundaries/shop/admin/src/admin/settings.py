ca5ca1af6606681907a54bf6ad328dff29a33f35
Victor Login <batazor111@gmail.com>
2024-08-24 02:11:09 +0200

    shop-admin: set sub-path
    Signed-off-by: Victor Login <batazor111@gmail.com>

--- boundaries/shop/admin/src/admin/settings.py
+++ boundaries/shop/admin/src/admin/settings.py
@@ -23,6 +23,7 @@ LOGIN_URL = env("LOGIN_URL", default="http://127.0.0.1:3000/next/auth/login")
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+FORCE_SCRIPT_NAME = "/admin"
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
