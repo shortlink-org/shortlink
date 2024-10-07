12e769f1d000c38d02adf1fd492d08a770e55ab8
Victor Login <batazor111@gmail.com>
2024-10-07 04:42:37 +0300

    ui-kit: use InstantSearch
    Signed-off-by: Victor Login <batazor111@gmail.com>

--- boundaries/ui-monorepo/packages/ui-kit/src/ui/SearchForm/SearchForm.tsx
+++ boundaries/ui-monorepo/packages/ui-kit/src/ui/SearchForm/SearchForm.tsx
@@ -1,28 +1,24 @@
-import SearchIcon from '@mui/icons-material/Search'
-import MicIcon from '@mui/icons-material/Mic'
+import { liteClient as algoliasearch } from 'algoliasearch/lite';
+import { InstantSearch, SearchBox, RefinementList } from 'react-instantsearch';
+
+const searchClient = algoliasearch('YourApplicationID', 'YourSearchOnlyAPIKey')
 
 function SearchForm() {
   return (
-    <div className="relative flex-grow">
-      <button
-        type="button"
-        className="absolute left-2 top-1/2 transform -translate-y-1/2 p-1 text-gray-500 hover:text-gray-700"
-      >
-        <SearchIcon className="h-5 w-5" />
-      </button>
-      <input
-        id="search"
-        type="search"
-        className="w-full pl-10 p-2 text-sm text-gray-700 form-input bg-white border border-gray-300 rounded-md focus:ring-blue-500 focus:border-blue-500"
-        placeholder="Search..."
+    <InstantSearch searchClient={searchClient} indexName="instant_search" insights>
+      <SearchBox
+        placeholder="Product, brand, color, …"
+        classNames={{
+          root: 'p-3 shadow-sm',
+          form: 'relative',
+          input: 'block w-full pl-9 pr-3 py-2 bg-white border border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 rounded-md focus:ring-1',
+          submitIcon: 'absolute top-0 left-0 bottom-0 w-6',
+        }}
       />
-      <button
-        type="submit"
-        className="absolute right-2 top-1/2 transform -translate-y-1/2 p-1 text-gray-500 hover:text-gray-700"
-      >
-        <MicIcon />
-      </button>
-    </div>
+      <RefinementList
+        attribute="categories"
+      />
+    </InstantSearch>
   )
 }
 
