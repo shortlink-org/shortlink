d81c85947399de4f737ffa7363daed89b868b32c
Viktor Login <batazor111@gmail.com>
2024-10-21 04:24:01 +0300

    helm: add makefile for generate values.schema.json
    Signed-off-by: Viktor Login <batazor111@gmail.com>

--- ops/Makefile/k8s/helm.mk
+++ ops/Makefile/k8s/helm.mk
@@ -1,7 +1,7 @@
 # KUBERNETES TASKS =====================================================================================================
 export HELM_EXPERIMENTAL_OCI=1
 
-deps:
+helm-deps:
 	@helm plugin install https://github.com/losisin/helm-values-schema-json.git
 
 helm-lint: ## Check Helm chart by linter
@@ -22,7 +22,7 @@ helm-docs: ### Generate HELM docs
 helm-upgrade: ### Upgrade all helm charts
 	@helm repo update
 	# Find all files named "Chart.yaml" in the current directory and its subdirectories
-	@find . -name "Chart.yaml" | xargs -I '{}' -P 8 bash -c ' \
+	@find ./ops/Helm -name "Chart.yaml" | xargs -I '{}' -P 8 bash -c ' \
             dir=$$(dirname "{}"); \
             cd "$$dir"; \
             rm Chart.lock || true; \
@@ -30,3 +30,17 @@ helm-upgrade: ### Upgrade all helm charts
         '
 
 	@make helm-docs
+
+.PHONY: helm-values-generate
+helm-values-generate: ### Generate or process values schema for all Helm charts
+	@find "$(CURDIR)/ops/Helm" -type f -name "Chart.yaml" -print0 | \
+	while IFS= read -r -d '' file; do \
+		dir="$$(dirname "$$file")"; \
+		echo "Processing directory: $$dir"; \
+		if [ -f "$$dir/.schema.yaml" ]; then \
+			echo "Generating values.schema.json in $$dir from .schema.yaml..."; \
+			cd "$$dir" && helm schema; \
+		else \
+			echo "No .schema.yaml found in $$dir, skipping..."; \
+		fi; \
+	done
