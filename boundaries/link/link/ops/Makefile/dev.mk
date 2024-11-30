2fdb550129da3f1d9e604b34ab90376845065d51
Viktor Login <batazor111@gmail.com>
2024-11-30 14:20:23 +0300

    link: add dev/down command for makefile
    Signed-off-by: Viktor Login <batazor111@gmail.com>

---
+++ boundaries/link/link/ops/Makefile/dev.mk
@@ -0,0 +1,16 @@
+# DEV ========================================================================
+dev: ### Run the development environment
+	@echo "Running the development environment..."
+	@COMPOSE_PROFILES=dns,observability,gateway docker compose \
+    		-f $(ROOT_DIR)/docker-compose.yaml \
+    		-f $(ROOT_DIR)/ops/docker-compose/tooling/services/coredns/coredns.yaml \
+    		-f $(ROOT_DIR)/ops/docker-compose/database/redis/redis.yaml \
+    		up -d --remove-orphans --build
+
+down: confirm ## Down docker compose
+	@COMPOSE_PROFILES=dns,observability,gateway docker compose \
+		-f $(ROOT_DIR)/docker-compose.yaml \
+		-f $(ROOT_DIR)/ops/docker-compose/tooling/services/coredns/coredns.yaml \
+		-f $(ROOT_DIR)/ops/docker-compose/database/redis/redis.yaml \
+	down --remove-orphans
+	@docker network prune -f
